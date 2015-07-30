12b4b6648fcf00d0b1ae89a73867591d260a8298
clearostranslations <translations@clearfoundation.com>
2015-07-29 21:57:56 -0400

    Updated translations

--- language/en_US/network_visualiser_lang.php
+++ language/en_US/network_visualiser_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['network_visualiser_app_description'] = 'The Network Visualiser app captures and displays data flow traversing your network in real-time, displaying source, destination and protocol information in addition to either bandwidth usage or total data captured over an interval.';
-$lang['network_visualiser_app_name'] = 'Network Visualiser';
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app captures and displays data flow traversing your network in real-time, displaying source, destination and protocol information in addition to either bandwidth usage or total data captured over an interval.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
 $lang['network_visualiser_display'] = 'Display';
 $lang['network_visualiser_display_invalid'] = 'Display is invalid.';
 $lang['network_visualiser_interval'] = 'Update Interval (seconds)';
@@ -11,7 +11,6 @@ $lang['network_visualiser_report_detailed'] = 'Tabular - Detailed';
 $lang['network_visualiser_report_graphical'] = 'Graphical';
 $lang['network_visualiser_report_invalid'] = 'Report type is invalid.';
 $lang['network_visualiser_report_simple'] = 'Tabular - Simple';
-$lang['network_visualiser_top_users'] = 'Top Consumers';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Consumers';
 $lang['network_visualiser_total_transfer'] = 'Total Transfer';
-$lang['network_visualiser_bandwidth'] = 'Bandwidth';
 $lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
