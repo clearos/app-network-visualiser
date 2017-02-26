94166209162c69691d1ad9019e7e6e406666033d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'App ujame in prikazovalniki pretok podatkov, ki vozijo po svoje omrežje v realnem času, ki prikazuje vir, namembnosti in informacij protokol poleg obeh pasovne širine uporabe ali vseh podatkov, posnetih po presledku Mreža Vizualizacija.';
+$lang['network_visualiser_app_name'] = 'mreža Vizualizacija';
+$lang['network_visualiser_display'] = 'zaslon';
+$lang['network_visualiser_display_invalid'] = 'Zaslon je neveljavna.';
+$lang['network_visualiser_interval'] = 'Update interval (v sekundah)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval je neveljavna.';
+$lang['network_visualiser_report'] = 'Oblika poročila';
+$lang['network_visualiser_report_detailed'] = 'Tabelarični - Podrobno';
+$lang['network_visualiser_report_graphical'] = 'grafični';
+$lang['network_visualiser_report_invalid'] = 'Vrsta poročila je neveljavna.';
+$lang['network_visualiser_report_simple'] = 'Tabelarični - Enostavno';
+$lang['network_visualiser_top_users'] = 'Top Potrošniki pasovne širine';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Mreža Traffic Povzetek';
