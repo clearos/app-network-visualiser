ae36626f9b0029318d42f7cb39f6f173fe7b25bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'အဆိုပါကွန်ယက် Visual app ကိုဖမ်းယူနိုင်ခြင်းနှင့် display တွေဒေတာစီးဆင်းမှု bandwidth ကိုအသုံးပြုမှုသို့မဟုတ်ကြားကာလကျော်ဖမ်းမိစုစုပေါင်းဒေတာဖြစ်ဖြစ်အပြင်အရင်းအမြစ်, ဦးတည်ရာနှင့် protocol ကိုသတင်းအချက်အလက်ပြသနေ, Real-time ၌သင်တို့၏ကွန်ယက်ကိုဖြတ်သန်း။';
+$lang['network_visualiser_app_name'] = 'Network ကို Visual';
+$lang['network_visualiser_display'] = 'ပြသ';
+$lang['network_visualiser_display_invalid'] = 'display မမှန်ကန်ဘူး။';
+$lang['network_visualiser_interval'] = 'Update ကို Interval (စက္ကန့်)';
+$lang['network_visualiser_interval_is_invalid'] = 'interval မမှန်ကန်ဘူး။';
+$lang['network_visualiser_report'] = 'အစီရင်ခံစာပုံစံ';
+$lang['network_visualiser_report_detailed'] = 'tabular - အသေးစိတ်';
+$lang['network_visualiser_report_graphical'] = 'graphical';
+$lang['network_visualiser_report_invalid'] = 'အစီရင်ခံစာပုံစံမမှန်ကန်ဘူး။';
+$lang['network_visualiser_report_simple'] = 'tabular - ရိုးရှင်းသော';
+$lang['network_visualiser_top_users'] = 'ထိပ်တန်း Bandwidth စားသုံးသူတွေ';
+$lang['network_visualiser_total_transfer'] = 'စုစုပေါင်းလွှဲပြောင်း';
+$lang['network_visualiser_traffic_summary'] = 'Network ကိုယာဉ်အသွားအလာအကျဉ်းချုပ်';
