eaca26a43c8c28eae85cd93948c04f43219e5cc2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:02:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Rețeaua de Visualizer captează app și afișează fluxul de date parcurgând rețeaua în timp real, sursa afișarea, destinație și informațiile de protocol, în plus față de utilizarea lățimii de bandă, fie sau datele totale capturate într-un interval.';
+$lang['network_visualiser_app_name'] = 'reţeaua de Visualizer';
+$lang['network_visualiser_display'] = 'Afişa';
+$lang['network_visualiser_display_invalid'] = 'Afișare este nevalid.';
+$lang['network_visualiser_interval'] = 'Interval de actualizare (secunde)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervalul este nevalid.';
+$lang['network_visualiser_report'] = 'Formatul raportului';
+$lang['network_visualiser_report_detailed'] = 'Tabular - detaliată';
+$lang['network_visualiser_report_graphical'] = 'Grafic';
+$lang['network_visualiser_report_invalid'] = 'Tipul de raport este nevalid.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simplu';
+$lang['network_visualiser_top_users'] = 'Top latime de banda Consumatorii';
+$lang['network_visualiser_total_transfer'] = 'Transfer total';
+$lang['network_visualiser_traffic_summary'] = 'Rețeaua de trafic Sumar';
