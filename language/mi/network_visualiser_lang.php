6844cbb098e6959cb15df2c0cf3b2cd0e4a039d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Nga kitea ana taupānga Whatunga Visualizer me ngā whakaaturanga rere raraunga kopikopiko koutou whatunga i roto i te wā-tūturu, whakaatu pūtake, ūnga me te mōhiohio kawa i roto i te tua ki rānei whakamahi hōkaiipurangi raraunga katoa riro i runga i te wā ranei.';
+$lang['network_visualiser_app_name'] = 'Visualizer whatunga';
+$lang['network_visualiser_display'] = 'whakaatu';
+$lang['network_visualiser_display_invalid'] = 'He muhu Whakaatu.';
+$lang['network_visualiser_interval'] = 'Whakahōu Wā (hēkona)';
+$lang['network_visualiser_interval_is_invalid'] = 'He muhu Wā.';
+$lang['network_visualiser_report'] = 'Hōputu Report';
+$lang['network_visualiser_report_detailed'] = 'Ripa - Detailed';
+$lang['network_visualiser_report_graphical'] = 'kauwhata';
+$lang['network_visualiser_report_invalid'] = 'He muhu momo Report.';
+$lang['network_visualiser_report_simple'] = 'Ripa - Simple';
+$lang['network_visualiser_top_users'] = 'Top Aratuku kiritaki';
+$lang['network_visualiser_total_transfer'] = 'Whakawhiti tapeke';
+$lang['network_visualiser_traffic_summary'] = 'Whakarāpopoto Traffic Whatunga';
