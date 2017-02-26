1547f6ca9634537d7bc451d016ef601bf44bc843
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network bisualak aplikazioa harrapaketak eta erakustaldiak datu fluxua zure sarea zeharkatzeko denbora errealean, erakusteak source, helmuga eta protokoloa informazioa bai banda zabalera erabilera edo tartean harrapatu datu guztira gain.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Bistan da baliozkoa.';
+$lang['network_visualiser_interval'] = 'Eguneratze-tartea (segundotan)';
+$lang['network_visualiser_interval_is_invalid'] = 'Tartearekin baliogabea da.';
+$lang['network_visualiser_report'] = 'txostena Format';
+$lang['network_visualiser_report_detailed'] = 'Taula - xehatua';
+$lang['network_visualiser_report_graphical'] = 'grafikoa';
+$lang['network_visualiser_report_invalid'] = 'Txosten mota ez da baliozkoa.';
+$lang['network_visualiser_report_simple'] = 'Taula - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bytes Kontsumitzaileak';
+$lang['network_visualiser_total_transfer'] = 'Transferentzia Guztira';
+$lang['network_visualiser_traffic_summary'] = 'Network Trafiko Laburpena';
