24b0480cd9a5fdeb6fa358e024a3745c967b332c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'У дадатак захоплівае і адлюстроўвае струмень дадзеных, які праходзіць праз вашу сетку ў рэжыме рэальнага часу, якая адлюстроўвае крыніца, месца прызначэння і інфармацыю пратаколу ў дадатак да любой прапускной здольнасці або выкарыстання агульных дадзеных, захопленых на працягу інтэрвалу Сетка Visualizer.';
+$lang['network_visualiser_app_name'] = 'сетка Visualizer';
+$lang['network_visualiser_display'] = 'дысплей';
+$lang['network_visualiser_display_invalid'] = 'Дысплей з&#39;яўляецца несапраўдным.';
+$lang['network_visualiser_interval'] = 'Інтэрвал абнаўлення (у секундах)';
+$lang['network_visualiser_interval_is_invalid'] = 'Інтэрвал з&#39;яўляецца несапраўдным.';
+$lang['network_visualiser_report'] = 'фармат справаздачы';
+$lang['network_visualiser_report_detailed'] = 'Таблічны - Дэталёвы';
+$lang['network_visualiser_report_graphical'] = 'графічны';
+$lang['network_visualiser_report_invalid'] = 'Тып справаздачы з&#39;яўляецца несапраўдным.';
+$lang['network_visualiser_report_simple'] = 'Таблічны - Просты';
+$lang['network_visualiser_top_users'] = 'Лепшыя спажыўцы Прапускная здольнасць';
+$lang['network_visualiser_total_transfer'] = 'поўная перадача';
+$lang['network_visualiser_traffic_summary'] = 'Сеткавы трафік Рэзюмэ';
