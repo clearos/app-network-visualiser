9de145dbd5d219eb120bb0c21826ab724ddcc009
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Network Visualizer programu captures na maonyesho data mtiririko apitaye mtandao wako katika muda halisi, kwa kuonyesha chanzo, marudio na maelezo itifaki kwa kuongeza ama matumizi Bandwidth au data jumla alitekwa juu ya muda.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'kuonyesha';
+$lang['network_visualiser_display_invalid'] = 'Kuonyesha ni batili.';
+$lang['network_visualiser_interval'] = 'Mwisho Interval (sekunde)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval ni batili.';
+$lang['network_visualiser_report'] = 'ripoti Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Kina';
+$lang['network_visualiser_report_graphical'] = 'graphical';
+$lang['network_visualiser_report_invalid'] = 'Ripoti ya aina ni batili.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Wikipedia';
+$lang['network_visualiser_top_users'] = 'Juu Bandwidth Wateja';
+$lang['network_visualiser_total_transfer'] = 'Jumla ya Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
