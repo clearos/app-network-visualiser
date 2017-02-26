f4d6ea4c6f6cac95bd218d0c531a9eebbffd2a12
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ქსელი Visualizer app captures და მონიტორები მონაცემების ნაკადი გადიოდა თქვენი ქსელის რეალურ დროში, ჩვენებისას წყარო, დანიშნულების და პროტოკოლის ინფორმაციის გარდა არც სიჩქარის გამოყენების ან სულ მონაცემები ტყვედ მეტი ინტერვალით.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'ჩვენება';
+$lang['network_visualiser_display_invalid'] = 'ჩვენების არასწორია.';
+$lang['network_visualiser_interval'] = 'განახლების ინტერვალი (წამი)';
+$lang['network_visualiser_interval_is_invalid'] = 'ინტერვალი არასწორია.';
+$lang['network_visualiser_report'] = 'ანგარიში ფორმატი';
+$lang['network_visualiser_report_detailed'] = 'ცხრილური - დეტალური';
+$lang['network_visualiser_report_graphical'] = 'გრაფიკული';
+$lang['network_visualiser_report_invalid'] = 'ანგარიშის ტიპი არასწორია.';
+$lang['network_visualiser_report_simple'] = 'ცხრილური - მარტივი';
+$lang['network_visualiser_top_users'] = 'ყველაზე გამტარუნარიანობა მომხმარებელთა';
+$lang['network_visualiser_total_transfer'] = 'სულ გადარიცხვა';
+$lang['network_visualiser_traffic_summary'] = 'საგზაო ქსელი რეზიუმე';
