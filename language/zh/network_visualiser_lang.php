28b7271775bbe95d11ddcb6f6ef1c940e63f6ebc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = '网络展示台应用捕获和显示除了任一带宽使用或捕获以上的间隔的总数据的数据流实时遍历网络，显示源，目的地和协议信息。';
+$lang['network_visualiser_app_name'] = '网络可视化';
+$lang['network_visualiser_display'] = '显示';
+$lang['network_visualiser_display_invalid'] = '显示无效。';
+$lang['network_visualiser_interval'] = '更新时间间隔（秒）';
+$lang['network_visualiser_interval_is_invalid'] = '时间间隔是无效的。';
+$lang['network_visualiser_report'] = '报告格式';
+$lang['network_visualiser_report_detailed'] = '表格 - 详细';
+$lang['network_visualiser_report_graphical'] = '图形';
+$lang['network_visualiser_report_invalid'] = '报告类型无效。';
+$lang['network_visualiser_report_simple'] = '表格 - 简单';
+$lang['network_visualiser_top_users'] = '顶级带宽消费者';
+$lang['network_visualiser_total_transfer'] = '共转移';
+$lang['network_visualiser_traffic_summary'] = '网络流量摘要';
