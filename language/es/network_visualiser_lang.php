57f44fe01d2bcdb340638c877bbf315267e039fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Las capturas de aplicaciones de red de visualizadores y pantallas de flujo de datos que atraviesa su red en tiempo real, mostrando fuente, destino y protocolo de información, además de ya sea el uso de ancho de banda o total de datos capturados en un intervalo.';
+$lang['network_visualiser_app_name'] = 'red visualizador';
+$lang['network_visualiser_display'] = 'Monitor';
+$lang['network_visualiser_display_invalid'] = 'Pantalla no es válido.';
+$lang['network_visualiser_interval'] = 'Intervalo de actualización (segundos)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervalo no es válido.';
+$lang['network_visualiser_report'] = 'Formato de informe';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Detallado';
+$lang['network_visualiser_report_graphical'] = 'Gráfico';
+$lang['network_visualiser_report_invalid'] = 'tipo de informe no es válido.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Los consumidores de ancho de banda superiores';
+$lang['network_visualiser_total_transfer'] = 'Transferencia total';
+$lang['network_visualiser_traffic_summary'] = 'Resumen de red de tráfico';
