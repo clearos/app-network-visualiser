af907f0be8202f5d1f7ba59a57059140669c0aa1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Mreža Visualizer aplikacija snima i prikazuje protok podataka poprijeko vaše mreže u stvarnom vremenu, prikazujući izvor, odredište i informacije protokol uz bilo propusnost korištenje ili ukupno podataka zarobljeni preko intervala.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'prikaz';
+$lang['network_visualiser_display_invalid'] = 'Ekran je nevažeći.';
+$lang['network_visualiser_interval'] = 'Update Interval (sekundi)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval je nevažeći.';
+$lang['network_visualiser_report'] = 'izvještaj Format';
+$lang['network_visualiser_report_detailed'] = 'Tabelarni - Detaljni';
+$lang['network_visualiser_report_graphical'] = 'grafički';
+$lang['network_visualiser_report_invalid'] = 'tip izvještaj je nevažeći.';
+$lang['network_visualiser_report_simple'] = 'Tabelarni - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Potrošači';
+$lang['network_visualiser_total_transfer'] = 'Ukupno Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Pregled';
