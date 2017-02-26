e3473aeca9bda0312e2026a009c75d6e34e8f709
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ап улавя Network Visualizer и дисплеи за потока от данни преминаващи вашата мрежа в реално време, показвайки източник, дестинация и информационни протокол, в допълнение към или използването на пропускателната способност или общо данни заловените през интервал.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'показ';
+$lang['network_visualiser_display_invalid'] = 'Display е невалиден.';
+$lang['network_visualiser_interval'] = 'Актуализиране на интервала (секунди)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval е невалиден.';
+$lang['network_visualiser_report'] = 'Доклад Format';
+$lang['network_visualiser_report_detailed'] = 'Таблични - Детайлен';
+$lang['network_visualiser_report_graphical'] = 'графически';
+$lang['network_visualiser_report_invalid'] = 'Вид доклад е невалиден.';
+$lang['network_visualiser_report_simple'] = 'Таблични - Simple';
+$lang['network_visualiser_top_users'] = 'Топ Bandwidth Потребителите';
+$lang['network_visualiser_total_transfer'] = 'Общо Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Обобщение на трафика';
