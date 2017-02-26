e344ddd854582221380390b1bb7265b30531a381
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'नेटवर्क Visualizer एप्लिकेशन को दर्शाता है और प्रदर्शित करता है या तो बैंडविड्थ उपयोग या कुल डेटा एक अंतराल पर कब्जा कर लिया है के अलावा वास्तविक समय में अपने नेटवर्क से गुजर डाटा प्रवाह, प्रदर्शित स्रोत, गंतव्य और प्रोटोकॉल के बारे में जानकारी।';
+$lang['network_visualiser_app_name'] = 'नेटवर्क Visualizer';
+$lang['network_visualiser_display'] = 'प्रदर्शन';
+$lang['network_visualiser_display_invalid'] = 'प्रदर्शन अमान्य है।';
+$lang['network_visualiser_interval'] = 'अद्यतन अंतराल (सेकंड)';
+$lang['network_visualiser_interval_is_invalid'] = 'अंतराल अमान्य है।';
+$lang['network_visualiser_report'] = 'रिपोर्ट स्वरूप';
+$lang['network_visualiser_report_detailed'] = 'सारणीबद्ध - विस्तृत';
+$lang['network_visualiser_report_graphical'] = 'चित्रात्मक';
+$lang['network_visualiser_report_invalid'] = 'रिपोर्ट प्रकार अमान्य है।';
+$lang['network_visualiser_report_simple'] = 'सारणीबद्ध - सरल';
+$lang['network_visualiser_top_users'] = 'शीर्ष बैंडविड्थ उपभोक्ताओं';
+$lang['network_visualiser_total_transfer'] = 'कुल स्थानांतरण';
+$lang['network_visualiser_traffic_summary'] = 'नेटवर्क यातायात सारांश';
