a07f3f9de579eaec0c9ae5c1b080756591ae92a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:01:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'नेटवर्क दृश्यकार अनुप्रयोग captures र ब्यान्डविथ प्रयोग वा अन्तराल भन्दा कब्जा कुल डाटा त अलावा डाटा प्रवाह वास्तविक-समयमा आफ्नो नेटवर्क traversing, प्रदर्शन स्रोत, गन्तव्य र प्रोटोकल जानकारी प्रदर्शित हुन्छ।';
+$lang['network_visualiser_app_name'] = 'नेटवर्क दृश्यकार';
+$lang['network_visualiser_display'] = 'प्रदर्शन';
+$lang['network_visualiser_display_invalid'] = 'प्रदर्शन अमान्य छ।';
+$lang['network_visualiser_interval'] = 'अंतराल (सेकेन्ड) अपडेट';
+$lang['network_visualiser_interval_is_invalid'] = 'अन्तराल अमान्य छ।';
+$lang['network_visualiser_report'] = 'स्वरूप रिपोर्ट';
+$lang['network_visualiser_report_detailed'] = 'टेबुलर - विस्तृत';
+$lang['network_visualiser_report_graphical'] = 'ग्राफिकल';
+$lang['network_visualiser_report_invalid'] = 'रिपोर्ट प्रकार अमान्य छ।';
+$lang['network_visualiser_report_simple'] = 'टेबुलर - सरल';
+$lang['network_visualiser_top_users'] = 'शीर्ष ब्यान्डविथ उपभोक्ताहरु';
+$lang['network_visualiser_total_transfer'] = 'कुल स्थानान्तरण';
+$lang['network_visualiser_traffic_summary'] = 'नेटवर्क ट्राफिक सारांश';
