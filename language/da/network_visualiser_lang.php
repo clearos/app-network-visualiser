b1b3e148af26e61df4443456634943a95e1ad93f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:55:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'De Network Visualizer app opfanger og viser data flow gennemkører dit netværk i realtid, visning kilde, destination og protokol oplysninger ud over enten båndbredde eller totale data fanget over et interval.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Skærm';
+$lang['network_visualiser_display_invalid'] = 'Display er ugyldig.';
+$lang['network_visualiser_interval'] = 'Opdater Interval (sekunder)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval er ugyldig.';
+$lang['network_visualiser_report'] = 'Rapportformat';
+$lang['network_visualiser_report_detailed'] = 'Tabelform - Detaljeret';
+$lang['network_visualiser_report_graphical'] = 'Grafisk';
+$lang['network_visualiser_report_invalid'] = 'Rapport er ugyldig.';
+$lang['network_visualiser_report_simple'] = 'Tabelform - Simple';
+$lang['network_visualiser_top_users'] = 'Top Båndbredde Forbrugere';
+$lang['network_visualiser_total_transfer'] = 'Samlet Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Netværk Trafik Summary';
