d12ecd465f3e66159b343ac7608aab0ffe16da06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'நெட்வொர்க் விஷூவலைஸர் பயன்பாட்டை பிடிக்கப்பட்ட மற்றும் காட்சிகள் அலைவரிசையை பயன்பாடு அல்லது ஒரு இடைவெளி கைப்பற்றியது மொத்த தரவு ஒன்று கூடுதலாக நிகழ் நேர உங்கள் பிணைய பயணித்து தரவு ஓட்டம், காண்பிக்கும் மூல, இலக்கு மற்றும் நெறிமுறை தகவல்.';
+$lang['network_visualiser_app_name'] = 'நெட்வொர்க் விஷூவலைஸர்';
+$lang['network_visualiser_display'] = 'காட்சி';
+$lang['network_visualiser_display_invalid'] = 'காட்சி தவறானது.';
+$lang['network_visualiser_interval'] = 'இடைவேளை (வினாடிகள்) புதுப்பிக்கவும்';
+$lang['network_visualiser_interval_is_invalid'] = 'இடைவேளை தவறானது.';
+$lang['network_visualiser_report'] = 'அறிக்கை வடிவம்';
+$lang['network_visualiser_report_detailed'] = 'வட்ட - மாதத்';
+$lang['network_visualiser_report_graphical'] = 'வரைகலை';
+$lang['network_visualiser_report_invalid'] = 'அறிக்கை வகை தவறானது.';
+$lang['network_visualiser_report_simple'] = 'வட்ட - எளிய';
+$lang['network_visualiser_top_users'] = 'சிறந்த அலைவரிசை நுகர்வோர்கள்';
+$lang['network_visualiser_total_transfer'] = 'மொத்த மாற்றம்';
+$lang['network_visualiser_traffic_summary'] = 'நெட்வொர்க் போக்குவரத்து சுருக்கம்';
