9bdfd97bb1494ed487a10c589d3397d8b8bb1c0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Ցանցը Visualizer ծրագիրը գրավում եւ ցուցադրում տվյալների հոսքը հասնող ձեր ցանցը իրական ժամանակում, ցուցաբերելով աղբյուրի, նպատակակետին եւ արձանագրային տեղեկատվություն `ի լրումն կամ թողունակության օգտագործման կամ ընդհանուր տվյալների առեւանգված ավելի քան մեկ պարբերականությամբ.';
+$lang['network_visualiser_app_name'] = 'Ցանցի Visualizer';
+$lang['network_visualiser_display'] = 'ցուցադրել';
+$lang['network_visualiser_display_invalid'] = 'Ցուցադրել անվավեր է:';
+$lang['network_visualiser_interval'] = 'Թարմացնել ժամանակամիջոցը (վայրկյան)';
+$lang['network_visualiser_interval_is_invalid'] = 'Ինտերվալ է անվավեր:';
+$lang['network_visualiser_report'] = 'Report Ֆորմատ';
+$lang['network_visualiser_report_detailed'] = 'Աղյուսակային - մանրամասն';
+$lang['network_visualiser_report_graphical'] = 'գրաֆիկական';
+$lang['network_visualiser_report_invalid'] = 'Զեկույցի տեսակ է անվավեր:';
+$lang['network_visualiser_report_simple'] = 'Աղյուսակային - Simple';
+$lang['network_visualiser_top_users'] = 'Ամենաշատ Թողունակության Սպառողների';
+$lang['network_visualiser_total_transfer'] = 'ընդհանուր Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Ցանցի Traffic Ամփոփում';
