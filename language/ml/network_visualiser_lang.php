6fdf1581898452fe8f8d9ab3f39ebbfd5d8a2ca8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'നെറ്റ്വർക്ക് വിഷ്വലൈസർ അപ്ലിക്കേഷൻ കാപ്ച്വറുകൾ പ്രദർശിപ്പിക്കുകയും ഡാറ്റ ഒഴുക്ക്, തത്സമയം നിങ്ങളുടെ നെറ്റ്വർക്ക് traversing ബാൻഡ്വിഡ്ത്ത് ഉപയോഗം അല്ലെങ്കിൽ ഒരു ഇടവേള മേൽ പിടിച്ചടക്കി ആകെ ഡാറ്റ ഒന്നുകിൽ പുറമേ ഉറവിടം, ലക്ഷ്യസ്ഥാനം, പ്രോട്ടോക്കോൾ വിവരങ്ങൾ പ്രദർശിപ്പിക്കുന്നത്.';
+$lang['network_visualiser_app_name'] = 'നെറ്റ്വർക്ക് വിഷ്വലൈസർ';
+$lang['network_visualiser_display'] = 'പ്രദർശിപ്പിക്കുക';
+$lang['network_visualiser_display_invalid'] = 'പ്രദർശിപ്പിക്കുക അസാധുവാണ്.';
+$lang['network_visualiser_interval'] = 'അപ്ഡേറ്റ് ഇടവേള (നിമിഷങ്ങൾ)';
+$lang['network_visualiser_interval_is_invalid'] = 'ഇടവേള അസാധുവാണ്.';
+$lang['network_visualiser_report'] = 'റിപ്പോർട്ട് ഫോർമാറ്റ്';
+$lang['network_visualiser_report_detailed'] = 'പട്ടികാ - വിശദമായ';
+$lang['network_visualiser_report_graphical'] = 'ഗ്രാഫിക്കൽ';
+$lang['network_visualiser_report_invalid'] = 'റിപ്പോർട്ട് തരം അസാധുവാണ്.';
+$lang['network_visualiser_report_simple'] = 'പട്ടികാ - ലളിത';
+$lang['network_visualiser_top_users'] = 'ടോപ്പ് ബാൻഡ്വിഡ്ത്ത് കൺസ്യൂമേഴ്സ്';
+$lang['network_visualiser_total_transfer'] = 'ആകെ ട്രാൻസ്ഫർ';
+$lang['network_visualiser_traffic_summary'] = 'നെറ്റ്വർക്ക് ട്രാഫിക് ചുരുക്കം';
