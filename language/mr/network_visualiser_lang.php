66631555bce8f730e12ad54a6b40f382e1a58b02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'नेटवर्क व्हिज्युअलायझर अनुप्रयोग कॅप्चर आणि दाखवतो बँडविड्थ वापर किंवा अंतराने प्रती मिळविले एकूण डेटा एकतर व्यतिरिक्त रिअल-टाइम मध्ये आपल्या नेटवर्क traversing डेटा प्रवाह प्रदर्शित स्रोत, गंतव्य आणि प्रोटोकॉल माहिती.';
+$lang['network_visualiser_app_name'] = 'नेटवर्क व्हिज्युअलायझर';
+$lang['network_visualiser_display'] = 'प्रदर्शन';
+$lang['network_visualiser_display_invalid'] = 'प्रदर्शन अवैध आहे.';
+$lang['network_visualiser_interval'] = 'मध्यांतर (सेकंद) अद्यतनित करा';
+$lang['network_visualiser_interval_is_invalid'] = 'मध्यांतर अवैध आहे.';
+$lang['network_visualiser_report'] = 'अहवाल स्वरूप';
+$lang['network_visualiser_report_detailed'] = 'तक्त्याच्या - माहिती';
+$lang['network_visualiser_report_graphical'] = 'ग्राफिकल';
+$lang['network_visualiser_report_invalid'] = 'अहवाल प्रकार अवैध आहे.';
+$lang['network_visualiser_report_simple'] = 'तक्त्याच्या - साधे';
+$lang['network_visualiser_top_users'] = 'शीर्ष बँडविड्थ ग्राहकांना';
+$lang['network_visualiser_total_transfer'] = 'एकूण ट्रान्सफर';
+$lang['network_visualiser_traffic_summary'] = 'नेटवर्क वाहतूक सारांश';
