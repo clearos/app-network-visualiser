5daec17f76c211a1266d304a807a9b114afa8805
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:02:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Мрежа Висуализер апликација бележи и приказује ток података који пролази своју мрежу у реалном времену, приказујући извор, одредиште и Информатион Протоцол поред или пропусни опсег употребе или укупних података заробљених у интервалу.';
+$lang['network_visualiser_app_name'] = 'mreža Визуализатор';
+$lang['network_visualiser_display'] = 'Приказ';
+$lang['network_visualiser_display_invalid'] = 'Екран је неважећи.';
+$lang['network_visualiser_interval'] = 'Упдате интервал (сецондс)';
+$lang['network_visualiser_interval_is_invalid'] = 'Интервал је неважећи.';
+$lang['network_visualiser_report'] = 'izveštaj Формат';
+$lang['network_visualiser_report_detailed'] = 'Табела - Детаљан';
+$lang['network_visualiser_report_graphical'] = 'графички';
+$lang['network_visualiser_report_invalid'] = 'Извештај тип је неважећи.';
+$lang['network_visualiser_report_simple'] = 'Табела - Једноставно';
+$lang['network_visualiser_top_users'] = 'Топ Бандвидтх Потрошачи';
+$lang['network_visualiser_total_transfer'] = 'укупна';
+$lang['network_visualiser_traffic_summary'] = 'Мрежни саобраћај Преглед';
