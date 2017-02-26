2999b23bc7b25f15ea51e90deca9aed51477debb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Mae&#39;r cipio app Network Visualizer ac arddangosfeydd llif data croesi eich rhwydwaith mewn amser real, gan ddangos ffynhonnell, cyrchfan a gwybodaeth protocol yn ogystal â naill ai defnydd lled band neu gyfanswm ddata dal dros egwyl.';
+$lang['network_visualiser_app_name'] = 'rhwydwaith Visualizer';
+$lang['network_visualiser_display'] = 'arddangos';
+$lang['network_visualiser_display_invalid'] = 'Arddangos yn annilys.';
+$lang['network_visualiser_interval'] = 'Diweddariad Ysbaid (eiliadau)';
+$lang['network_visualiser_interval_is_invalid'] = 'Egwyl yn annilys.';
+$lang['network_visualiser_report'] = 'Ffurf yr adroddiad';
+$lang['network_visualiser_report_detailed'] = 'Tablau - Manwl';
+$lang['network_visualiser_report_graphical'] = 'graffigol';
+$lang['network_visualiser_report_invalid'] = 'math o adroddiad yn annilys.';
+$lang['network_visualiser_report_simple'] = 'Tablau - Syml';
+$lang['network_visualiser_top_users'] = 'Top Defnyddwyr Lled Band';
+$lang['network_visualiser_total_transfer'] = 'Cyfanswm Trosglwyddo';
+$lang['network_visualiser_traffic_summary'] = 'Rhwydwaith Crynodeb Traffig';
