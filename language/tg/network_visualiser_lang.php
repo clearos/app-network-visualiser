b4a2f4c88a602284cfff5f823d89bf79c3e0556e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Дар сурати барнома Шабакаи Visualizer ва намоиши ҷараёни маълумоти ҷанобат шабакаи шумо дар вақти воқеӣ, сарчашмаи намоиши, таъинот ва иттилооти протокол ба ғайр аз ё истифодаи маҷрои ё маълумотњои љамъбастї гирифташуда бар фосилаи.';
+$lang['network_visualiser_app_name'] = 'шабакаи Visualizer';
+$lang['network_visualiser_display'] = 'намоиш';
+$lang['network_visualiser_display_invalid'] = 'Дисплей нодуруст аст.';
+$lang['network_visualiser_interval'] = 'Навсозии фосилавӣ (сония)';
+$lang['network_visualiser_interval_is_invalid'] = 'Фосилаи нодуруст аст.';
+$lang['network_visualiser_report'] = 'Њисобот оид ба Формат';
+$lang['network_visualiser_report_detailed'] = 'Tabular - муфассали';
+$lang['network_visualiser_report_graphical'] = 'графикӣ';
+$lang['network_visualiser_report_invalid'] = 'Њисобот оид ба намуди нодуруст аст.';
+$lang['network_visualiser_report_simple'] = 'Tabular - оддӣ';
+$lang['network_visualiser_top_users'] = 'Top маҷрои истеъмолкунандагон';
+$lang['network_visualiser_total_transfer'] = 'Њамагї интиқоли';
+$lang['network_visualiser_traffic_summary'] = 'Шабакаи хулоса трафик';
