d9e20c218556a176e4e60d90f5d454303cd9ccdf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Network Bildeffekter app fångar och visar dataflödes korsar nätverket i realtid och visar källan, målet och protokollinformation utöver antingen bandbredd eller totala data som samlats in under ett intervall.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Visa';
+$lang['network_visualiser_display_invalid'] = 'Display är ogiltig.';
+$lang['network_visualiser_interval'] = 'Uppdateringsintervall (sekunder)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervallet är ogiltigt.';
+$lang['network_visualiser_report'] = 'rapportformat';
+$lang['network_visualiser_report_detailed'] = 'Tabell - Detaljerad';
+$lang['network_visualiser_report_graphical'] = 'grafiska';
+$lang['network_visualiser_report_invalid'] = 'Rapporttypen är ogiltig.';
+$lang['network_visualiser_report_simple'] = 'Tabell - Enkel';
+$lang['network_visualiser_top_users'] = 'Sammanfattning Bandbredds Konsumenter';
+$lang['network_visualiser_total_transfer'] = 'totala Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Sammanfattning';
