347acfae69472ecd6f6bf8e1423a604f5ba1d934
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:06:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Uhlelo Network Isibonisi lokusebenza kunqanda futhi ubonisa idatha flow elizulazula inethiwekhi yakho in real-time, umthombo ukubonisa, uya khona nolwazi protocol ngaphezu noma ukusetshenziswa komkhawulokudonsa noma idatha Imininingwane ethumba phezu esingaba.';
+$lang['network_visualiser_app_name'] = 'Network Isibonisi';
+$lang['network_visualiser_display'] = 'Khombisa';
+$lang['network_visualiser_display_invalid'] = 'Khombisa ayivumelekile.';
+$lang['network_visualiser_interval'] = 'Buyekeza isikhawu (imizuzwana)';
+$lang['network_visualiser_interval_is_invalid'] = 'Sokuphumula ayivumelekile.';
+$lang['network_visualiser_report'] = 'Umbiko Ifomethi';
+$lang['network_visualiser_report_detailed'] = 'Tabular - okuningiliziwe';
+$lang['network_visualiser_report_graphical'] = 'isikhombimsebenzisi';
+$lang['network_visualiser_report_invalid'] = 'Umbiko uhlobo alivumelekile.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top Abathengi Bandwidth';
+$lang['network_visualiser_total_transfer'] = 'Dlulisa Total';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
