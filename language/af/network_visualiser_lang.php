342573428d14af14c08d65c24977ff8b1f760d0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Die Netwerk Visualizer app vang en vertoon data vloei dwars deur jou netwerk in real-time, vertoon bron, bestemming en protokol inligting bykomend tot óf bandwydte gebruik of totale vasgevang oor &#39;n interval data.';
+$lang['network_visualiser_app_name'] = 'netwerk Visualizer';
+$lang['network_visualiser_display'] = 'Wys';
+$lang['network_visualiser_display_invalid'] = 'Vertoning is ongeldig.';
+$lang['network_visualiser_interval'] = 'Update interval (sekondes)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval is ongeldig.';
+$lang['network_visualiser_report'] = 'verslagformaat';
+$lang['network_visualiser_report_detailed'] = 'Tabel - Gedetailleerde';
+$lang['network_visualiser_report_graphical'] = 'grafiese';
+$lang['network_visualiser_report_invalid'] = 'tipe verslag is ongeldig.';
+$lang['network_visualiser_report_simple'] = 'Tabel - Eenvoudige';
+$lang['network_visualiser_top_users'] = 'Top Bandwydte Verbruikers';
+$lang['network_visualiser_total_transfer'] = 'totale oordrag';
+$lang['network_visualiser_traffic_summary'] = 'Netwerkverkeer Opsomming';
