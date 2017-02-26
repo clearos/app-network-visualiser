c36d6b9ba6632545b1f17c2afa70977e90c02ab3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Network quod Visualizer App ostendit capit, et transiens notitia network fluxus in real-time, ostentans principium, et finem in protocollo praeter notitia, aut consuetudine Sed data vel capta per totum spatium.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Display est invalidum.';
+$lang['network_visualiser_interval'] = 'Update Intervallum (seconds)';
+$lang['network_visualiser_interval_is_invalid'] = 'Nullum est spatium.';
+$lang['network_visualiser_report'] = 'Format Report';
+$lang['network_visualiser_report_detailed'] = 'Expositio - Detailed';
+$lang['network_visualiser_report_graphical'] = 'graphical';
+$lang['network_visualiser_report_invalid'] = 'Relatio autem est invalidum.';
+$lang['network_visualiser_report_simple'] = 'Expositio - Vicipaedia';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Perussi';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Summary network traffic';
