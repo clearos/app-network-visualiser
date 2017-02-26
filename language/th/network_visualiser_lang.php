d1ab3e0d9fded074cc349ead6fe03d6b1f8b9e2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'เครือข่าย Visualizer จับแอปและการแสดงการไหลของข้อมูลภายในเครือข่ายของคุณในเวลาจริงแหล่งที่มาแสดงปลายทางและข้อมูลโปรโตคอลนอกเหนือไปจากทั้งการใช้งานแบนด์วิดธ์หรือข้อมูลทั้งหมดที่ถูกจับกว่าช่วงเวลา';
+$lang['network_visualiser_app_name'] = 'เครือข่าย Visualizer';
+$lang['network_visualiser_display'] = 'แสดง';
+$lang['network_visualiser_display_invalid'] = 'ดิสเพลย์ไม่ถูกต้อง';
+$lang['network_visualiser_interval'] = 'อัพเดทช่วงเวลา (วินาที)';
+$lang['network_visualiser_interval_is_invalid'] = 'ช่วงเวลาที่ไม่ถูกต้อง';
+$lang['network_visualiser_report'] = 'รูปแบบรายงาน';
+$lang['network_visualiser_report_detailed'] = 'ตาราง - รายละเอียด';
+$lang['network_visualiser_report_graphical'] = 'กราฟิก';
+$lang['network_visualiser_report_invalid'] = 'ประเภทรายงานไม่ถูกต้อง';
+$lang['network_visualiser_report_simple'] = 'ตาราง - ง่าย';
+$lang['network_visualiser_top_users'] = 'ผู้บริโภค Bandwidth สูงสุด';
+$lang['network_visualiser_total_transfer'] = 'รวมบริการรถรับส่ง';
+$lang['network_visualiser_traffic_summary'] = 'สรุปเครือข่ายการจราจร';
