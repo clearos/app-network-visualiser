7b0edbaff98252dea896f42bebda69045f3348de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:55:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'La Reto visualizador app kaptoj kaj montras datumojn fluo petolanta via reto en reala tempo, montranta fonto, celloko kaj protokolo informo krom ĉu bandwidth uzado aŭ tuta datumo kaptita super intervalo.';
+$lang['network_visualiser_app_name'] = 'reto visualizador';
+$lang['network_visualiser_display'] = 'montriĝo';
+$lang['network_visualiser_display_invalid'] = 'Montriĝo estas nevalida.';
+$lang['network_visualiser_interval'] = 'Ĝisdatigi Intervalo (sekundoj)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervalo estas nevalida.';
+$lang['network_visualiser_report'] = 'Raporti Formato';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Detalaj';
+$lang['network_visualiser_report_graphical'] = 'grafikaj';
+$lang['network_visualiser_report_invalid'] = 'Raporto tipo estas nevalida.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simpla';
+$lang['network_visualiser_top_users'] = 'Supro Bandwidth Konsumantoj';
+$lang['network_visualiser_total_transfer'] = 'entute Translokigo';
+$lang['network_visualiser_traffic_summary'] = 'Reto Trafiko Resumo';
