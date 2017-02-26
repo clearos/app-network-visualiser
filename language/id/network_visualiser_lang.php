7fa7d896bc78b58dad36b33c23e3a46bed4e6c34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Jaringan Visualizer menangkap aplikasi dan menampilkan aliran data melintasi jaringan Anda secara real-time, menampilkan sumber, tujuan dan informasi protokol selain baik penggunaan bandwidth atau total data yang diambil melalui selang.';
+$lang['network_visualiser_app_name'] = 'jaringan Visualizer';
+$lang['network_visualiser_display'] = 'pameran';
+$lang['network_visualiser_display_invalid'] = 'Tampilan tidak valid.';
+$lang['network_visualiser_interval'] = 'Memperbarui Interval (detik)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval tidak valid.';
+$lang['network_visualiser_report'] = 'laporan Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Rinci';
+$lang['network_visualiser_report_graphical'] = 'Grafis';
+$lang['network_visualiser_report_invalid'] = 'Jenis laporan tidak valid.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Sederhana';
+$lang['network_visualiser_top_users'] = 'Bandwidth Konsumen Top';
+$lang['network_visualiser_total_transfer'] = 'Jumlah transfer';
+$lang['network_visualiser_traffic_summary'] = 'Jaringan Ringkasan Lalu Lintas';
