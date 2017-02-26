27c2ab718e43417ace16061c49a6e5bd92556e9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Các ảnh chụp ứng dụng Mạng Visualizer và hiển thị dòng dữ liệu đi qua mạng của bạn trong thời gian thực, nguồn hiển thị, điểm đến và các thông tin giao thức ngoài hoặc sử dụng băng thông hoặc tổng số dữ liệu bị bắt trong một khoảng thời gian.';
+$lang['network_visualiser_app_name'] = 'mạng Visualizer';
+$lang['network_visualiser_display'] = 'Trưng bày';
+$lang['network_visualiser_display_invalid'] = 'Hiển thị là không hợp lệ.';
+$lang['network_visualiser_interval'] = 'Cập nhật Interval (giây)';
+$lang['network_visualiser_interval_is_invalid'] = 'Khoảng thời gian không hợp lệ.';
+$lang['network_visualiser_report'] = 'báo cáo Format';
+$lang['network_visualiser_report_detailed'] = 'Dạng bảng - Chi tiết';
+$lang['network_visualiser_report_graphical'] = 'đồ họa';
+$lang['network_visualiser_report_invalid'] = 'Báo cáo loại là không hợp lệ.';
+$lang['network_visualiser_report_simple'] = 'Dạng bảng - Đơn giản';
+$lang['network_visualiser_top_users'] = 'Băng thông tiêu dùng Lên trên';
+$lang['network_visualiser_total_transfer'] = 'Tổng Chuyển';
+$lang['network_visualiser_traffic_summary'] = 'Mạng Tóm tắt giao thông';
