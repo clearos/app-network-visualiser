b65175e700e78e68e74107e4006b4eb80209eb42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:01:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'تصویر برداری نرم افزار شبکه ویژوالایزر و نمایش جریان داده تراورس شبکه خود را در زمان واقعی، منبع نمایش، مقصد و اطلاعات پروتکل علاوه بر استفاده از پهنای باند هم و یا در کل داده ها اسیر بیش از یک فاصله است.';
+$lang['network_visualiser_app_name'] = 'شبکه ویژوالایزر';
+$lang['network_visualiser_display'] = 'نمایش دادن';
+$lang['network_visualiser_display_invalid'] = 'ها نامعتبر است.';
+$lang['network_visualiser_interval'] = 'به روز رسانی فاصله (ثانیه)';
+$lang['network_visualiser_interval_is_invalid'] = 'فاصله نامعتبر است.';
+$lang['network_visualiser_report'] = 'فرمت گزارش';
+$lang['network_visualiser_report_detailed'] = 'بشقاب - تفصیلی';
+$lang['network_visualiser_report_graphical'] = 'گرافیکی';
+$lang['network_visualiser_report_invalid'] = 'نوع گزارش نامعتبر است.';
+$lang['network_visualiser_report_simple'] = 'بشقاب - ساده';
+$lang['network_visualiser_top_users'] = 'بالا پهنای باند مصرف کنندگان';
+$lang['network_visualiser_total_transfer'] = 'مجموع انتقال';
+$lang['network_visualiser_traffic_summary'] = 'شبکه خلاصه ترافیک';
