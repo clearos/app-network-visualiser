8b9c39df0ec19cc74a894041f21ebaf2301c3f72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Желі Visualizer Қолданба басып алу және сайтыңызда деректер ағыны өткізу жолағын пайдалану немесе аралықта түсірілген жалпы деректер, не қосымша көзі, тағайындау және хаттама ақпаратты визуализацияланған, нақты уақытта желіні қиып.';
+$lang['network_visualiser_app_name'] = 'Желі Visualizer';
+$lang['network_visualiser_display'] = 'көрсету';
+$lang['network_visualiser_display_invalid'] = 'Дисплей жарамсыз болып табылады.';
+$lang['network_visualiser_interval'] = 'Жаңарту аралығы (секунд)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval жарамсыз болып табылады.';
+$lang['network_visualiser_report'] = 'есеп пішімі';
+$lang['network_visualiser_report_detailed'] = 'Кестелік - Толық';
+$lang['network_visualiser_report_graphical'] = 'Графикалық';
+$lang['network_visualiser_report_invalid'] = 'Есеп түрі жарамсыз болып табылады.';
+$lang['network_visualiser_report_simple'] = 'Кестелік - Қарапайым';
+$lang['network_visualiser_top_users'] = 'Жоғары Өткізу жолағы Тұтынушылар';
+$lang['network_visualiser_total_transfer'] = 'Барлығы Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Желілік Traffic Резюме';
