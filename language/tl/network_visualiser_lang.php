6eb99884155fa5507ea664fa6ab2f072a97ea979
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Ang kinukuha Network Visualizer app at nagpapakita ng daloy ng data traversing iyong network sa real-time, ipinapakita ang source, patutunguhang at protocol impormasyon bilang karagdagan sa alinman ang paggamit ng bandwidth o kabuuang data nakunan sa ibabaw ng isang interval.';
+$lang['network_visualiser_app_name'] = 'network Visualizer';
+$lang['network_visualiser_display'] = 'display';
+$lang['network_visualiser_display_invalid'] = 'Display ay hindi wasto.';
+$lang['network_visualiser_interval'] = 'I-update ang Palugit (segundo)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval ay hindi wasto.';
+$lang['network_visualiser_report'] = 'Isumbong Format';
+$lang['network_visualiser_report_detailed'] = 'Hugis ng mga talaan - Detalyadong';
+$lang['network_visualiser_report_graphical'] = 'graphical';
+$lang['network_visualiser_report_invalid'] = 'Uri ng ulat ay hindi wasto.';
+$lang['network_visualiser_report_simple'] = 'Hugis ng mga talaan - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Consumers';
+$lang['network_visualiser_total_transfer'] = 'Kabuuang Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Traffic Buod Network';
