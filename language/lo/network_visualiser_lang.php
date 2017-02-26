145b75e9b497d071887dea08624bc7c7b6bbedbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ເຄືອຂ່າຍ Visualizer captures app ແລະການສະແດງໄຫລຂອງຂໍ້ມູນ traversing ເຄືອຂ່າຍຂອງທ່ານໃນການທີ່ແທ້ຈິງທີ່ໃຊ້ເວລາ, ແຫຼ່ງສະແດງ, ຈຸດຫມາຍປາຍທາງແລະຂໍ້ມູນຂ່າວສານອະນຸສັນຍາໃນນອກຈາກນັ້ນບໍ່ວ່າຈະນໍາໃຊ້ເວັບໄຊຫລືຂໍ້ມູນທັງຫມົດ captured ໃນໄລຍະໄລຍະຫ່າງເປັນ.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'ການສະແດງ';
+$lang['network_visualiser_display_invalid'] = 'ການສະແດງແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['network_visualiser_interval'] = 'ປັບປຸງໄລຍະຫ່າງ (ວິນາທີ)';
+$lang['network_visualiser_interval_is_invalid'] = 'ໄລຍະຫ່າງແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['network_visualiser_report'] = 'ຮູບແບບບົດລາຍງານ';
+$lang['network_visualiser_report_detailed'] = 'ຕາຕະລາງ - ລະອຽດ';
+$lang['network_visualiser_report_graphical'] = 'ຮູບພາບ';
+$lang['network_visualiser_report_invalid'] = 'ປະເພດບົດລາຍງານແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['network_visualiser_report_simple'] = 'ຕາຕະລາງ - ງ່າຍດາຍ';
+$lang['network_visualiser_top_users'] = 'ຜູ້ບໍລິໂພກ Bandwidth Top';
+$lang['network_visualiser_total_transfer'] = 'ທັງຫມົດການຖ່າຍໂອນ';
+$lang['network_visualiser_traffic_summary'] = 'ເຄືອຂ່າຍ Summary ການຈະລາຈອນ';
