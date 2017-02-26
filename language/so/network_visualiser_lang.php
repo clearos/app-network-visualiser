529fdd5b9dee693a7bc0d98f493e571b0d297749
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer wareegtay app iyo qaab socodka xogta oo Toggooda aad network in-waqtiga dhabta ah, il bandhigeen, caga iyo macluumaad maamuuska lagu daro labada isticmaalka bandwidth ama macluumaadka guud ee la qabtay in ka badan bareeg ah.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Display waa baadil.';
+$lang['network_visualiser_interval'] = 'Cusboonaysii bareeg (ilbiriqsi)';
+$lang['network_visualiser_interval_is_invalid'] = 'Bareeg waa baadil.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - faahfaasan';
+$lang['network_visualiser_report_graphical'] = 'Garaafka';
+$lang['network_visualiser_report_invalid'] = 'nooca Report waa baadil.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top bandwidth Macaamiisha';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Kooban Traffic';
