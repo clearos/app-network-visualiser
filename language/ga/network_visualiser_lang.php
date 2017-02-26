cef40f8063bd61496081aef897797e566e304226
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Na gabhálacha app Líonra Visualizer agus taispeántais sreabhadh sonraí traversing do líonra i bhfíor-am, foinse taispeáint, ceann scríbe agus faisnéis prótacal chomh maith le ceachtar úsáid bandaleithead nó sonraí iomlán a gabhadh thar eatramh.';
+$lang['network_visualiser_app_name'] = 'líonra Visualizer';
+$lang['network_visualiser_display'] = 'Taispeáin';
+$lang['network_visualiser_display_invalid'] = 'Is Taispeáin neamhbhailí.';
+$lang['network_visualiser_interval'] = 'Update Eatramh (soicindí)';
+$lang['network_visualiser_interval_is_invalid'] = 'Is Eatramh neamhbhailí.';
+$lang['network_visualiser_report'] = 'Tuairisc Formáid';
+$lang['network_visualiser_report_detailed'] = 'Tháblach - mionsonraithe';
+$lang['network_visualiser_report_graphical'] = 'grafach';
+$lang['network_visualiser_report_invalid'] = 'Is cineál tuairisce neamhbhailí.';
+$lang['network_visualiser_report_simple'] = 'Tháblach - Simplí';
+$lang['network_visualiser_top_users'] = 'Barr Bandaleithid Tomhaltóirí';
+$lang['network_visualiser_total_transfer'] = 'Aistriú iomlán';
+$lang['network_visualiser_traffic_summary'] = 'Achoimre Tráchta Líonra';
