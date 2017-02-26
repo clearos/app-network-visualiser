221dd8ac29b0daeffedbc1e45cbf7e52442e862a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ネットワークビジュアライザアプリをキャプチャして表示し、データフローの帯域幅使用量や間隔で撮影し、全データのいずれかに加えて、送信元、送信先、およびプロトコル情報を表示し、リアルタイムでネットワークを横断します。';
+$lang['network_visualiser_app_name'] = 'ネットワークビジュアライザ';
+$lang['network_visualiser_display'] = '表示';
+$lang['network_visualiser_display_invalid'] = '表示は無効です。';
+$lang['network_visualiser_interval'] = '更新間隔（秒）';
+$lang['network_visualiser_interval_is_invalid'] = '間隔が無効です。';
+$lang['network_visualiser_report'] = 'レポート形式';
+$lang['network_visualiser_report_detailed'] = '表形式 - 詳細';
+$lang['network_visualiser_report_graphical'] = 'グラフィカル';
+$lang['network_visualiser_report_invalid'] = 'レポートの種類が無効です。';
+$lang['network_visualiser_report_simple'] = '表形式 - シンプルな';
+$lang['network_visualiser_top_users'] = 'トップ帯域幅の消費者';
+$lang['network_visualiser_total_transfer'] = '総転送';
+$lang['network_visualiser_traffic_summary'] = 'ネットワークトラフィックの概要';
