b735bbca23d12cb0e274a4935df455cc2a90ec29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Tarmoq Visualizer ilova egallashga va ko&#39;rsatadi tarmoqli kengligi foydalanish yoki oradan ustida qo&#39;lga umumiy ma&#39;lumotlar yo tashqari ma&#39;lumotlar real vaqt rejimida tarmoq çaprazlayan oqimini, aks manbasini, maqsad va protokol ma&#39;lumot.';
+$lang['network_visualiser_app_name'] = 'tarmoq Visualizer';
+$lang['network_visualiser_display'] = 'displey';
+$lang['network_visualiser_display_invalid'] = 'Displey haqiqiy emas.';
+$lang['network_visualiser_interval'] = 'Interval (soniya) yangilash';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval haqiqiy emas.';
+$lang['network_visualiser_report'] = 'hisobot formati';
+$lang['network_visualiser_report_detailed'] = 'Singlisini - Mukammal';
+$lang['network_visualiser_report_graphical'] = 'grafik';
+$lang['network_visualiser_report_invalid'] = 'Hisobot turi yaroqsiz hisoblanadi.';
+$lang['network_visualiser_report_simple'] = 'Singlisini - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Iste&#39;molchilar';
+$lang['network_visualiser_total_transfer'] = 'jami uzatish';
+$lang['network_visualiser_traffic_summary'] = 'Tarmoq trafik Summary';
