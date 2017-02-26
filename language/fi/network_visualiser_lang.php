c4d10153ba0c4570cf70f5dc55cdaa01a2b75664
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:55:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Verkosto Visualizer sovellus tallentaa ja näyttää tietovirta liikkumisesta verkkoon reaaliaikaisesti, näytetään lähde, kohde ja protokolla tietojen lisäksi joko kaistanleveyttä tai koko kerättyä yli välein.';
+$lang['network_visualiser_app_name'] = 'verkko Visualizer';
+$lang['network_visualiser_display'] = 'Näyttö';
+$lang['network_visualiser_display_invalid'] = 'Näyttö on virheellinen.';
+$lang['network_visualiser_interval'] = 'Päivitä Interval (sekuntia)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval on virheellinen.';
+$lang['network_visualiser_report'] = 'raportti muoto';
+$lang['network_visualiser_report_detailed'] = 'Taulukkomuodossa - Yksityiskohtainen';
+$lang['network_visualiser_report_graphical'] = 'Graafinen';
+$lang['network_visualiser_report_invalid'] = 'Raportti ei kelpaa.';
+$lang['network_visualiser_report_simple'] = 'Taulukkomuodossa - Yksinkertainen';
+$lang['network_visualiser_top_users'] = 'Top kaistanleveys Kuluttajat';
+$lang['network_visualiser_total_transfer'] = 'Yhteensä siirto';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Yhteenveto';
