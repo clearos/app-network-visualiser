70fdacce9be49535c209349063b1b43286415a79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Il jaqbad app Netwerk Visualizer u displays fluss ta &#39;dejta jaqsmu network tiegħek fil-ħin reali, sors wiri, destinazzjoni u informazzjoni protokoll minbarra li jew użu bandwidth jew data total maqbuda fuq intervall.';
+$lang['network_visualiser_app_name'] = 'netwerk Visualizer';
+$lang['network_visualiser_display'] = 'wiri';
+$lang['network_visualiser_display_invalid'] = 'Wiri huwa invalidu.';
+$lang['network_visualiser_interval'] = 'Aġġorna l Intervall (f&#39;sekondi)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervall huwa invalidu.';
+$lang['network_visualiser_report'] = 'rapport Format';
+$lang['network_visualiser_report_detailed'] = 'Tabulari - dettaljati';
+$lang['network_visualiser_report_graphical'] = 'grafika';
+$lang['network_visualiser_report_invalid'] = 'tip rapport huwa invalidu.';
+$lang['network_visualiser_report_simple'] = 'Tabulari - Sempliċi';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Konsumaturi';
+$lang['network_visualiser_total_transfer'] = 'Trasferiment totali';
+$lang['network_visualiser_traffic_summary'] = 'Netwerk Sommarju Traffiku';
