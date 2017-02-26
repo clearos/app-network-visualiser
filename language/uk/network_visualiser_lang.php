2f778800e03c216d0b4dd93eb593c724938fbefc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'В додаток захоплює і відображає потік даних, що проходить через вашу мережу в режимі реального часу, що відображає джерело, місце призначення і інформацію протоколу на додаток до будь-якої пропускної здатності або використання загальних даних, захоплених протягом інтервалу Мережа Visualizer.';
+$lang['network_visualiser_app_name'] = 'мережа Visualizer';
+$lang['network_visualiser_display'] = 'дисплей';
+$lang['network_visualiser_display_invalid'] = 'Дисплей є недійсним.';
+$lang['network_visualiser_interval'] = 'Частота оновлення (в секундах)';
+$lang['network_visualiser_interval_is_invalid'] = 'Інтервал є недійсним.';
+$lang['network_visualiser_report'] = 'формат звіту';
+$lang['network_visualiser_report_detailed'] = 'Табличний - Детальний';
+$lang['network_visualiser_report_graphical'] = 'графічний';
+$lang['network_visualiser_report_invalid'] = 'Тип звіту є недійсним.';
+$lang['network_visualiser_report_simple'] = 'Табличний - Простий';
+$lang['network_visualiser_top_users'] = 'Кращі споживачі Пропускна здатність';
+$lang['network_visualiser_total_transfer'] = 'повна передача';
+$lang['network_visualiser_traffic_summary'] = 'Мережевий трафік Резюме';
