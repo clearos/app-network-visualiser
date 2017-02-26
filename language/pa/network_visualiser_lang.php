6aea2b7193be4ef2cc609acd1c5c6455d8d76510
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:01:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ਨੈੱਟਵਰਕ ਵਿਜ਼ੁਅਲਾਈਜ਼ਰ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਕੈਪਚਰ ਅਤੇ ਡਿਸਪਲੇਅ ਕਿਸੇ ਨੂੰ ਦਰਸਾਈ ਉਪਯੋਗ ਜ ਕੁੱਲ ਡਾਟਾ ਇੱਕ ਅੰਤਰਾਲ &#39;ਤੇ ਕਬਜ਼ਾ ਕਰ ਲਿਆ ਕਰਨ ਦੇ ਨਾਲ ਅਸਲੀ-ਟਾਈਮ ਵਿੱਚ ਆਪਣੇ ਨੈੱਟਵਰਕ ਗੁਜਰ ਡਾਟਾ ਵਹਾਅ, ਵੇਖਾਉਣ ਸਰੋਤ, ਮੰਜ਼ਿਲ ਹੈ ਅਤੇ ਪਰੋਟੋਕਾਲ ਜਾਣਕਾਰੀ.';
+$lang['network_visualiser_app_name'] = 'ਨੈੱਟਵਰਕ ਵਿਜ਼ੁਅਲਾਈਜ਼ਰ';
+$lang['network_visualiser_display'] = 'ਡਿਸਪਲੇਅ';
+$lang['network_visualiser_display_invalid'] = 'ਡਿਸਪਲੇਅ ਗਲਤ ਹੈ.';
+$lang['network_visualiser_interval'] = 'ਅੱਪਡੇਟ ਅੰਤਰਾਲ (ਸਕਿੰਟ)';
+$lang['network_visualiser_interval_is_invalid'] = 'ਅੰਤਰਾਲ ਗਲਤ ਹੈ.';
+$lang['network_visualiser_report'] = 'ਦੀ ਰਿਪੋਰਟ ਫਾਰਮੈਟ';
+$lang['network_visualiser_report_detailed'] = 'ਸਾਰਣਾ - ਵਿਸਤ੍ਰਿਤ';
+$lang['network_visualiser_report_graphical'] = 'ਗਰਾਫੀਕਲ';
+$lang['network_visualiser_report_invalid'] = 'ਦੀ ਰਿਪੋਰਟ ਦੀ ਕਿਸਮ ਗਲਤ ਹੈ.';
+$lang['network_visualiser_report_simple'] = 'ਸਾਰਣਾ - ਸਧਾਰਨ';
+$lang['network_visualiser_top_users'] = 'ਸਿਖਰ ਦਰਸਾਈ ਖਪਤਕਾਰ';
+$lang['network_visualiser_total_transfer'] = 'ਕੁੱਲ ਸੰਚਾਰ';
+$lang['network_visualiser_traffic_summary'] = 'ਨੈੱਟਵਰਕ ਟਰੈਫਿਕ ਸੰਖੇਪ';
