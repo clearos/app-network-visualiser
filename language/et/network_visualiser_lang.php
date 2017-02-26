0802617d43bcbeb9561ba37b92e759359a17bae0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:55:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Võrgustik visualiseerija app lööb ja kuvab andmevoo liiklevad oma võrgu reaalajas, kuvades vastuvõtja ning protokolli informatsiooni lisaks kas ribalaiuse kasutamist või kokku andmed pildistatud üle intervalliga.';
+$lang['network_visualiser_app_name'] = 'võrgu visualiseerija';
+$lang['network_visualiser_display'] = 'ekraan';
+$lang['network_visualiser_display_invalid'] = 'Näita ei sobi.';
+$lang['network_visualiser_interval'] = 'Värskendamise intervalli (sekundites)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervall on kehtetu.';
+$lang['network_visualiser_report'] = 'aruandevorm';
+$lang['network_visualiser_report_detailed'] = 'Tabeli - Detailne';
+$lang['network_visualiser_report_graphical'] = 'graafilised';
+$lang['network_visualiser_report_invalid'] = 'Aruande liik ei sobi.';
+$lang['network_visualiser_report_simple'] = 'Tabeli - Lihtne';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Tarbijad';
+$lang['network_visualiser_total_transfer'] = 'Kokku Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic kokkuvõte';
