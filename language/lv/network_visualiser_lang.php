76616c4d4bbc1e006e4184bedc40733c6c5ff650
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Tīkls Visualizer app notver un parāda datu plūsmas šķērso savu tīklu reālajā laikā, attēlot avotu, galamērķi un protokola informācija papildus nu joslas platuma lietojumu vai kopējiem datiem, kas uzņemti pa intervālu.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'displejs';
+$lang['network_visualiser_display_invalid'] = 'Displejs ir nederīgs.';
+$lang['network_visualiser_interval'] = 'Atjaunināt intervāls (sekundēs)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervāls ir nederīgs.';
+$lang['network_visualiser_report'] = 'ziņojums formāts';
+$lang['network_visualiser_report_detailed'] = 'Tabulveida - Detalizēts';
+$lang['network_visualiser_report_graphical'] = 'Grafiskais';
+$lang['network_visualiser_report_invalid'] = 'Pārskata veids ir nederīgs.';
+$lang['network_visualiser_report_simple'] = 'Tabulveida - Simple';
+$lang['network_visualiser_top_users'] = 'Top Joslas platums Patērētāji';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic kopsavilkums';
