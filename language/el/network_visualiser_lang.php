bc0978272dc6f94fe7d37b2c492b58838d8091ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:56:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Οι συλλήψεις εφαρμογή του Δικτύου Visualizer και εμφανίζει τη ροή δεδομένων που διέρχονται από το δίκτυό σας σε πραγματικό χρόνο, εμφανίζοντας την πηγή, τον προορισμό και πληροφορίες πρωτοκόλλου, επιπλέον της χρήσης του εύρους ζώνης ή ολική δεδομένα που συλλαμβάνονται πάνω από ένα διάστημα.';
+$lang['network_visualiser_app_name'] = 'Visualizer δίκτυο';
+$lang['network_visualiser_display'] = 'Απεικόνιση';
+$lang['network_visualiser_display_invalid'] = 'Display είναι άκυρη.';
+$lang['network_visualiser_interval'] = 'Ενημέρωση Διάστημα (δευτερόλεπτα)';
+$lang['network_visualiser_interval_is_invalid'] = 'Διάστημα δεν είναι έγκυρο.';
+$lang['network_visualiser_report'] = 'έκθεση Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Λεπτομερής';
+$lang['network_visualiser_report_graphical'] = 'Γραφικός';
+$lang['network_visualiser_report_invalid'] = 'Έκθεση είδος δεν είναι έγκυρο.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Απλό';
+$lang['network_visualiser_top_users'] = 'Top καταναλωτές Bandwidth';
+$lang['network_visualiser_total_transfer'] = 'Σύνολο Μεταφορά';
+$lang['network_visualiser_traffic_summary'] = 'Περίληψη της κυκλοφορίας του δικτύου';
