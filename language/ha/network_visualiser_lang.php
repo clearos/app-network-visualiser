df5c0982c6fc0eb0a6ad6c121deb7c38451fc726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:56:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app na kama da nuni data kwarara traversing afaretan cibiyar a hakikanin-lokaci, nuna source, manufa da kuma yarjejeniya bayanai a Bugu da kari ko dai bandwidth mai cutarwa ko total data kama a kan wani lokaci-lokaci.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'nuni';
+$lang['network_visualiser_display_invalid'] = 'Nuni ba daidai ba ne.';
+$lang['network_visualiser_interval'] = 'Sabunta tazara (seconds)';
+$lang['network_visualiser_interval_is_invalid'] = 'Tazara ba daidai ba ne.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - CIKAKKEN';
+$lang['network_visualiser_report_graphical'] = 'zana';
+$lang['network_visualiser_report_invalid'] = 'Report type ba daidai ba ne.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Masu amfani';
+$lang['network_visualiser_total_transfer'] = 'total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
