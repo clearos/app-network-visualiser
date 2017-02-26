3a01cadd13cad1ac114607e17e0d72b0f926fab8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:56:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Rezo Visualizer app kaptire yo epi montre done koule Travelers rezo w lan nan tan reyèl, ap montre sous, destinasyon ak enfòmasyon pwotokòl nan adisyon a swa l &#39;Pleasant oswa done manm pran sou yon entèval.';
+$lang['network_visualiser_app_name'] = 'Rezo Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Display se valab.';
+$lang['network_visualiser_interval'] = 'Mete ajou Entèval (segonn)';
+$lang['network_visualiser_interval_is_invalid'] = 'Entèval se valab.';
+$lang['network_visualiser_report'] = 'Rapò Fòma';
+$lang['network_visualiser_report_detailed'] = 'Tabulaire - detaye';
+$lang['network_visualiser_report_graphical'] = 'Grafik';
+$lang['network_visualiser_report_invalid'] = 'Rapò di ki kalite se valab.';
+$lang['network_visualiser_report_simple'] = 'Tabulaire - Senp';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Konsomatè';
+$lang['network_visualiser_total_transfer'] = 'Total Transfè';
+$lang['network_visualiser_traffic_summary'] = 'Rezo Rezime Trafik';
