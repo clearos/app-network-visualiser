1d2fa889224b4ce452ff997c6336db078a5ee7fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:06:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app ya ati ki o han data sisan traversing nẹtiwọki rẹ ni gidi-akoko, han orisun, nlo ati bèèrè alaye ni afikun si boya bandiwidi lilo tabi lapapọ data sile lori ohun ti aarin.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'àpapọ';
+$lang['network_visualiser_display_invalid'] = 'Àpapọ jẹ invalid.';
+$lang['network_visualiser_interval'] = 'Mu arin (aaya)';
+$lang['network_visualiser_interval_is_invalid'] = 'Aarin ti wa ni invalid.';
+$lang['network_visualiser_report'] = 'Iroyin kika';
+$lang['network_visualiser_report_detailed'] = 'Tabular - alaye';
+$lang['network_visualiser_report_graphical'] = 'atọkùn';
+$lang['network_visualiser_report_invalid'] = 'Iroyin iru ni invalid.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandiwidi onibara';
+$lang['network_visualiser_total_transfer'] = 'Total Gbe';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Lakotan';
