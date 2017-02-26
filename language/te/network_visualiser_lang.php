53c051e9011915cb58c948c67a923333c2cbb198
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:04:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'నెట్వర్క్ విజువలైజ అనువర్తనం బంధించి డిస్ప్లేలు బ్యాండ్విడ్త్ వినియోగం లేదా ఒక విరామం పైగా స్వాధీనం మొత్తం డేటా గాని అదనంగా డేటా ప్రవాహం వాస్తవ సమయంలో మీ నెట్వర్క్ నదీ ప్రవాహానికి అడ్డంగా ప్రయాణం మూలం ప్రదర్శించడం, గమ్యం మరియు ప్రోటోకాల్ సమాచారం.';
+$lang['network_visualiser_app_name'] = 'నెట్వర్క్ విజువలైజ';
+$lang['network_visualiser_display'] = 'ప్రదర్శన';
+$lang['network_visualiser_display_invalid'] = 'ప్రదర్శించు చెల్లదు.';
+$lang['network_visualiser_interval'] = 'నవీకరణ విరామం (సెకన్లు)';
+$lang['network_visualiser_interval_is_invalid'] = 'ఇంటర్వెల్ చెల్లదు.';
+$lang['network_visualiser_report'] = 'ఫార్మాట్ నివేదిక';
+$lang['network_visualiser_report_detailed'] = 'పట్టిక - వివరణాత్మక';
+$lang['network_visualiser_report_graphical'] = 'గ్రాఫికల్';
+$lang['network_visualiser_report_invalid'] = 'నివేదిక రకం చెల్లదు.';
+$lang['network_visualiser_report_simple'] = 'పట్టిక - సింపుల్';
+$lang['network_visualiser_top_users'] = 'టాప్ బ్యాండ్విడ్త్ కన్స్యూమర్స్';
+$lang['network_visualiser_total_transfer'] = 'మొత్తం ట్రాన్స్ఫర్';
+$lang['network_visualiser_traffic_summary'] = 'నెట్వర్క్ ట్రాఫిక్ సారాంశం';
