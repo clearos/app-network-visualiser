ea47689eca5614f6aa4f4b3a633898f2a9158d99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = '네트워크 시각화 응용 프로그램의 캡처 및 디스플레이 대역폭 사용 또는 간격으로 촬영 된 전체 데이터 중 하나에 추가 데이터를 실시간으로 네트워크를 통과하는 흐름을 표시 소스, 대상 및 프로토콜 정보를 제공합니다.';
+$lang['network_visualiser_app_name'] = '네트워크 시각화';
+$lang['network_visualiser_display'] = '디스플레이';
+$lang['network_visualiser_display_invalid'] = '디스플레이가 잘못되었습니다.';
+$lang['network_visualiser_interval'] = '간격 (초) 업데이트';
+$lang['network_visualiser_interval_is_invalid'] = '간격이 잘못되었습니다.';
+$lang['network_visualiser_report'] = '보고서 형식';
+$lang['network_visualiser_report_detailed'] = '테이블 형식 - 상세';
+$lang['network_visualiser_report_graphical'] = '그래픽';
+$lang['network_visualiser_report_invalid'] = '보고서 유형이 잘못되었습니다.';
+$lang['network_visualiser_report_simple'] = '테이블 형식 - 단순';
+$lang['network_visualiser_top_users'] = '상위 대역폭 소비자';
+$lang['network_visualiser_total_transfer'] = '총 전송';
+$lang['network_visualiser_traffic_summary'] = '네트워크 트래픽 요약';
