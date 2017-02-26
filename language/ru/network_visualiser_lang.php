8477fff58428ad5bb6d32365d7fd416bc0e3a061
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:02:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'В приложение захватывает и отображает поток данных, проходящий через вашу сеть в режиме реального времени, отображающую источник, место назначения и информацию протокола в дополнение к любой пропускной способности или использования общих данных, захваченных в течение интервала Сеть Visualizer.';
+$lang['network_visualiser_app_name'] = 'Сеть Visualizer';
+$lang['network_visualiser_display'] = 'дисплей';
+$lang['network_visualiser_display_invalid'] = 'Дисплей является недействительным.';
+$lang['network_visualiser_interval'] = 'Интервал обновления (в секундах)';
+$lang['network_visualiser_interval_is_invalid'] = 'Интервал является недействительным.';
+$lang['network_visualiser_report'] = 'Формат отчета';
+$lang['network_visualiser_report_detailed'] = 'Табличный - Детальный';
+$lang['network_visualiser_report_graphical'] = 'графический';
+$lang['network_visualiser_report_invalid'] = 'Тип отчета является недействительным.';
+$lang['network_visualiser_report_simple'] = 'Табличный - Простой';
+$lang['network_visualiser_top_users'] = 'Лучшие потребители Пропускная способность';
+$lang['network_visualiser_total_transfer'] = 'Полная передача';
+$lang['network_visualiser_traffic_summary'] = 'Сетевой трафик Резюме';
