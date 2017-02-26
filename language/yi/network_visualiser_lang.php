77fb2cd2528cb529a41d1b4fe2d9da791e77dc35
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'די נעץ וויסואַליזער אַפּ קאַפּטשערז און דיספּלייז דאַטע לויפן טראַווערסינג דיין נעץ אין פאַקטיש-מאָל, Displaying מקור, דעסטיניישאַן און פּראָטאָקאָל אינפֿאָרמאַציע אין דערצו צו אָדער באַנדווידט באַניץ אָדער גאַנץ דאַטן קאַפּטשערד איבער אַ מעהאַלעך.';
+$lang['network_visualiser_app_name'] = 'נעטוואָרק וויסואַליזער';
+$lang['network_visualiser_display'] = 'אַרויסווייַז';
+$lang['network_visualiser_display_invalid'] = 'אַרויסווייַזן איז פאַרקריפּלט.';
+$lang['network_visualiser_interval'] = 'אַפּדייט ינטערוואַל (סעקונדעס)';
+$lang['network_visualiser_interval_is_invalid'] = 'מעהאַלעך איז פאַרקריפּלט.';
+$lang['network_visualiser_report'] = 'באריכט פֿאָרמאַטירונג';
+$lang['network_visualiser_report_detailed'] = 'טאַבולאַר - דעטאַילעד';
+$lang['network_visualiser_report_graphical'] = 'גראַפיקאַל';
+$lang['network_visualiser_report_invalid'] = 'באריכט טיפּ איז פאַרקריפּלט.';
+$lang['network_visualiser_report_simple'] = 'טאַבולאַר - סימפּלע';
+$lang['network_visualiser_top_users'] = 'שפּיץ באַנדווידטה קאָנסומערס';
+$lang['network_visualiser_total_transfer'] = 'גאַנץ פֿאַרוואַנדלער';
+$lang['network_visualiser_traffic_summary'] = 'נעטוואָרק פאַרקער קיצער';
