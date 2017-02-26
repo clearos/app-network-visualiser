120304d9abc65ec6edebd807e555f0c106d49932
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Rangkaian Visualizer menangkap app dan memaparkan aliran data merentasi rangkaian anda dalam masa nyata, sumber memaparkan, destinasi dan maklumat protokol tambahan kepada sama ada penggunaan jalur lebar atau jumlah data yang dirakam lebih selang.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'paparan';
+$lang['network_visualiser_display_invalid'] = 'Paparan tidak sah.';
+$lang['network_visualiser_interval'] = 'Kemas kini Interval (saat)';
+$lang['network_visualiser_interval_is_invalid'] = 'Selang tidak sah.';
+$lang['network_visualiser_report'] = 'laporan Format';
+$lang['network_visualiser_report_detailed'] = 'Jadual - terperinci';
+$lang['network_visualiser_report_graphical'] = 'grafik';
+$lang['network_visualiser_report_invalid'] = 'Laporan jenis adalah tidak sah.';
+$lang['network_visualiser_report_simple'] = 'Jadual - Mudah';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Pengguna';
+$lang['network_visualiser_total_transfer'] = 'Jumlah Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Rangkaian Ringkasan Traffic';
