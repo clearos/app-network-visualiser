7aefa39ea995d3a08aaeedf5680ca8cb2ec8d8fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ការចាប់យកកម្មវិធីបណ្តាញដែលមើលឃើញនិងបង្ហាញទិន្នន័យការឆ្លងកាត់តំបន់បណ្តាញលំហូររបស់អ្នកនៅក្នុងពេលវេលាពិតប្រាកដ, ប្រភពបង្ហាញទិសដៅនិងពពិធីការក្នុងការបន្ថែមទៅទាំងការប្រើប្រាស់ល្បឿនឬទិន្នន័យសរុបចាប់យកនៅលើចន្លោះពេលមួយ។';
+$lang['network_visualiser_app_name'] = 'រូបភាពមើលឃើញបណ្តាញ';
+$lang['network_visualiser_display'] = 'បង្ហាញ';
+$lang['network_visualiser_display_invalid'] = 'បង្ហាញមិនត្រឹមត្រូវ។';
+$lang['network_visualiser_interval'] = 'ធ្វើឱ្យទាន់សម័យចន្លោះពេល (វិនាទី)';
+$lang['network_visualiser_interval_is_invalid'] = 'ចន្លោះពេលមិនត្រឹមត្រូវ។';
+$lang['network_visualiser_report'] = 'របាយការណ៍ទ្រង់ទ្រាយ';
+$lang['network_visualiser_report_detailed'] = 'តារាង - លម្អិត';
+$lang['network_visualiser_report_graphical'] = 'ក្រាហ្វិក';
+$lang['network_visualiser_report_invalid'] = 'របាយការណ៍ប្រភេទមិនត្រឹមត្រូវ។';
+$lang['network_visualiser_report_simple'] = 'តារាង - សាមញ្ញ';
+$lang['network_visualiser_top_users'] = 'កំពូលកម្រិតបញ្ជូនអតិថិជន';
+$lang['network_visualiser_total_transfer'] = 'សេវាផ្ទេរប្រាក់សរុប';
+$lang['network_visualiser_traffic_summary'] = 'សង្ខេបចរាចរណ៍បណ្តាញ';
