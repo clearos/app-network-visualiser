116dffaf60190f5a8f29d9036e8473036b8b0d67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app fangar og birtir gagnaflæði fara yfir net í rauntíma, sýna Source, áfangastað og siðareglur upplýsingar til viðbótar annaðhvort bandbreidd notkun eða heildarmagn gagna tekin yfir millibili.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'sýna';
+$lang['network_visualiser_display_invalid'] = 'Skjárinn er ógilt.';
+$lang['network_visualiser_interval'] = 'Uppfæra bili (sekúndur)';
+$lang['network_visualiser_interval_is_invalid'] = 'Bil er ógilt.';
+$lang['network_visualiser_report'] = 'skýrsla Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Ítarleg';
+$lang['network_visualiser_report_graphical'] = 'Myndræn';
+$lang['network_visualiser_report_invalid'] = 'Skýrsla er ógilt.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Einföld';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Neytendur';
+$lang['network_visualiser_total_transfer'] = 'alls Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Yfirlit';
