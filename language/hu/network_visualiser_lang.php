617239b5613a11b59eead048799707ff886d076f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'A Network Visualizer alkalmazás méri és jeleníti meg az adatokat átfolyó a hálózat valós idejű megjelenítésére forrás, cél és protokoll információkat mellett akár sávszélesség-használat vagy teljes rögzített adatokat időközzel.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Kijelző';
+$lang['network_visualiser_display_invalid'] = 'Display érvénytelen.';
+$lang['network_visualiser_interval'] = 'Frissítse intervallum (másodperc)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval érvénytelen.';
+$lang['network_visualiser_report'] = 'jelentés formátum';
+$lang['network_visualiser_report_detailed'] = 'Táblázatos - Részletes';
+$lang['network_visualiser_report_graphical'] = 'Grafikus';
+$lang['network_visualiser_report_invalid'] = 'Jelentés típusa érvénytelen.';
+$lang['network_visualiser_report_simple'] = 'Táblázatos - Egyszerű';
+$lang['network_visualiser_top_users'] = 'Top Sávszélesség fogyasztók';
+$lang['network_visualiser_total_transfer'] = 'Összesen Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Hálózati forgalom Összefoglaló';
