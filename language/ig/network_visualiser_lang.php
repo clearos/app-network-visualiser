7afb5e857d935ab540d4b5a919d2049771a481e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:57:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer ngwa captures na ngosipụta data eruba traversing netwọk gị na ezigbo oge, na-egosipụta isi iyi, na-aga ma protocol ọmụma na mgbakwunye na ma bandwit ojiji ma ọ bụ ngụkọta data weghaara n&#39;elu otu nkeji.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Display bụ ghara ịdị irè.';
+$lang['network_visualiser_interval'] = 'Imelite nkeji (Sekọnd)';
+$lang['network_visualiser_interval_is_invalid'] = 'Nkeji bụ ghara ịdị irè.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - zuru ezu';
+$lang['network_visualiser_report_graphical'] = 'onyeōzi';
+$lang['network_visualiser_report_invalid'] = 'Report ụdị bụ ghara ịdị irè.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwit bụghịkwa';
+$lang['network_visualiser_total_transfer'] = 'Total Nyefee';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
