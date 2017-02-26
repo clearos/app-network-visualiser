f2d51f0ba821dbada152bec0bf86693cd50bccab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:58:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ನೆಟ್ವರ್ಕ್ ವಿಶುವಲೈಜರ್ ಅಪ್ಲಿಕೇಶನ್ ವಶಪಡಿಸಿಕೊಂಡದ್ದು ಮತ್ತು ಪ್ರದರ್ಶನಗಳು ಬ್ಯಾಂಡ್ವಿಡ್ತ್ ಬಳಕೆ ಅಥವಾ ಮಧ್ಯಂತರ ಸೆರೆಹಿಡಿಯಲಾಯಿತು ಒಟ್ಟು ಡೇಟಾ ಎರಡೂ ಜೊತೆಗೆ ಮಾಹಿತಿ ಹರಿವಿನ ನೈಜ ಸಮಯದಲ್ಲಿ ನಿಮ್ಮ ಜಾಲ ಗ್ಯಾಲರಿ ಪ್ರದರ್ಶಿಸುವ ಮೂಲ, ಗಮ್ಯಸ್ಥಾನ ಮತ್ತು ಪ್ರೋಟೋಕಾಲ್ ಮಾಹಿತಿ.';
+$lang['network_visualiser_app_name'] = 'ನೆಟ್ವರ್ಕ್ ವಿಶುವಲೈಜರ್';
+$lang['network_visualiser_display'] = 'ಪ್ರದರ್ಶನ';
+$lang['network_visualiser_display_invalid'] = 'ಪ್ರದರ್ಶನ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['network_visualiser_interval'] = 'ಅಪ್ಡೇಟ್ ಮಧ್ಯಂತರ (ಸೆಕೆಂಡುಗಳಲ್ಲಿ)';
+$lang['network_visualiser_interval_is_invalid'] = 'ಮಧ್ಯಂತರ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['network_visualiser_report'] = 'ಸ್ವರೂಪ ವರದಿ';
+$lang['network_visualiser_report_detailed'] = 'ಕೋಷ್ಟಕ - ವಿವರವಾದ';
+$lang['network_visualiser_report_graphical'] = 'ಚಿತ್ರಾತ್ಮಕ';
+$lang['network_visualiser_report_invalid'] = 'ವರದಿ ಪ್ರಕಾರ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['network_visualiser_report_simple'] = 'ಕೋಷ್ಟಕ - ಸರಳ';
+$lang['network_visualiser_top_users'] = 'ಟಾಪ್ ಬ್ಯಾಂಡ್ವಿಡ್ತ್ ಗ್ರಾಹಕರು';
+$lang['network_visualiser_total_transfer'] = 'ಒಟ್ಟು ವರ್ಗಾವಣೆ';
+$lang['network_visualiser_traffic_summary'] = 'ನೆಟ್ವರ್ಕ್ ಸಂಚಾರ ಸಾರಾಂಶ';
