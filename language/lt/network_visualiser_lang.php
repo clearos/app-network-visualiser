dff66adb9175ce78442e29c92f56b47d0283ec5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Tinklo Visualizer app fiksuoja ir rodo duomenų srauto važiuojantiems savo tinklą realiu laiku, rodyti šaltinis, paskirties ir protokolo informaciją be abiejų srautą ar visų duomenų užfiksuoti virš intervalu.';
+$lang['network_visualiser_app_name'] = 'Tinklo Visualizer';
+$lang['network_visualiser_display'] = 'displėjus';
+$lang['network_visualiser_display_invalid'] = 'Ekranas yra neteisinga.';
+$lang['network_visualiser_interval'] = 'Atnaujinkite intervalas (sekundėmis)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervalas yra negaliojantis.';
+$lang['network_visualiser_report'] = 'ataskaitos forma';
+$lang['network_visualiser_report_detailed'] = 'Lentelių - Detalus';
+$lang['network_visualiser_report_graphical'] = 'grafinės';
+$lang['network_visualiser_report_invalid'] = 'Ataskaitos tipas yra neteisinga.';
+$lang['network_visualiser_report_simple'] = 'Lentelių - Paprasta';
+$lang['network_visualiser_top_users'] = 'Geriausi Bandwidth Vartotojai';
+$lang['network_visualiser_total_transfer'] = 'Iš viso perdavimas';
+$lang['network_visualiser_traffic_summary'] = 'Tinklo eismo santrauka';
