fd62f4257f73d0e5f89c927ea8d789bc8581194d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:59:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Ny Network Visualizer fampiharana hoe nambabo sy ny tahirin-kevitra mampiseho ny tambajotra mivezivezy amin&#39;ny mikoriana amin&#39;ny tena andro, maneho loharano, toerana sy ny Protocol vaovao, ankoatra ny passante na fampiasana na tanteraka rakitra voasambotra noho ny elanelam-potoana io.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Asehoy ny tsy manan-kery.';
+$lang['network_visualiser_interval'] = 'Vaovao farany elanelam-potoana io (seconds)';
+$lang['network_visualiser_interval_is_invalid'] = 'Elanelam-potoana io dia tsy manan-kery.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Detailed';
+$lang['network_visualiser_report_graphical'] = 'tsary ny';
+$lang['network_visualiser_report_invalid'] = 'Report karazana dia tsy manan-kery.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top passante Mpanjifa';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
