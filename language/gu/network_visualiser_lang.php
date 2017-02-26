82344c3398b38817c3178b891827e9b93848be81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:56:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'નેટવર્ક Visualizer એપ્લિકેશન મેળવે છે અને ક્યાં દર્શાવે બેન્ડવિડ્થ વપરાશ અથવા કુલ ડેટા એક અંતરાલ પર કબજે ઉપરાંત વાસ્તવિક સમય માં તમારા નેટવર્ક સરકાઉ ડેટાનો પ્રવાહ, પ્રદર્શિત સ્ત્રોત, સ્થળ અને પ્રોટોકોલ માહિતી.';
+$lang['network_visualiser_app_name'] = 'નેટવર્ક Visualizer';
+$lang['network_visualiser_display'] = 'ડિસ્પ્લે';
+$lang['network_visualiser_display_invalid'] = 'ડિસ્પ્લે અમાન્ય છે.';
+$lang['network_visualiser_interval'] = 'સુધારા અંતરાલ (સેકન્ડો)';
+$lang['network_visualiser_interval_is_invalid'] = 'અંતરાલ અમાન્ય છે.';
+$lang['network_visualiser_report'] = 'રિપોર્ટ ફોર્મેટ';
+$lang['network_visualiser_report_detailed'] = 'કોઠા - વિગતવાર';
+$lang['network_visualiser_report_graphical'] = 'ગ્રાફિકલ';
+$lang['network_visualiser_report_invalid'] = 'રિપોર્ટ પ્રકાર અમાન્ય છે.';
+$lang['network_visualiser_report_simple'] = 'કોઠા - સરળ';
+$lang['network_visualiser_top_users'] = 'ટોચના બેન્ડવીડ્થ ગ્રાહકો';
+$lang['network_visualiser_total_transfer'] = 'કુલ ટ્રાન્સફર';
+$lang['network_visualiser_traffic_summary'] = 'નેટવર્ક ટ્રાફિક સારાંશ';
