42ba984fb6a46dd87b64d967f9456b0d53ec5469
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'стан фаќа мрежа Визуелизатор и прикажува проток на податоци што минуваат низ мрежата во реално време, прикажување извор, дестинација и протокол информации во прилог на или пропусен опсег за употреба или вкупно податоци заробени повеќе од еден интервал.';
+$lang['network_visualiser_app_name'] = 'мрежа Визуелизатор';
+$lang['network_visualiser_display'] = 'Покажи';
+$lang['network_visualiser_display_invalid'] = 'Покажи не е валиден.';
+$lang['network_visualiser_interval'] = 'Update interval (секунди)';
+$lang['network_visualiser_interval_is_invalid'] = 'Интервал не е валиден.';
+$lang['network_visualiser_report'] = 'извештај формат';
+$lang['network_visualiser_report_detailed'] = 'Табеларен - Детален';
+$lang['network_visualiser_report_graphical'] = 'графички';
+$lang['network_visualiser_report_invalid'] = 'тип извештај не е валиден.';
+$lang['network_visualiser_report_simple'] = 'Табеларен - Википедија';
+$lang['network_visualiser_top_users'] = 'Топ пропусен опсег Потрошувачите';
+$lang['network_visualiser_total_transfer'] = 'вкупниот преносен';
+$lang['network_visualiser_traffic_summary'] = 'Краток преглед на мрежата на сообраќајот';
