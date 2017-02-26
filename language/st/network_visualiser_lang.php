01096a47534454e893ea0f94244c150b9c30f5aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app e hapang le bontšana ya data phallo tsamaea netweke ya hao ka sebele-nako, ho bontša mohloling oa bona, moo se eang teng le boitsebiso bo melaoana ho phaella ho ebang tšebeliso kgonothebolo kapa data ya palo yohle ya a hapa holim &#39;karohano e.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Display';
+$lang['network_visualiser_display_invalid'] = 'Display ha o sebetse.';
+$lang['network_visualiser_interval'] = 'Ntjhafatsa karohano (metsotsoana)';
+$lang['network_visualiser_interval_is_invalid'] = 'Karohano ha o sebetse.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tafola - e qaqileng';
+$lang['network_visualiser_report_graphical'] = 'dikerafike';
+$lang['network_visualiser_report_invalid'] = 'Report mofuta ha o sebetse.';
+$lang['network_visualiser_report_simple'] = 'Tafola - Simple';
+$lang['network_visualiser_top_users'] = 'Top kgonothebolo Bareki';
+$lang['network_visualiser_total_transfer'] = 'Total Ho fetiswa';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Summary';
