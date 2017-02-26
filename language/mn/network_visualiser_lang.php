dbfdee4aa933484c441d4263c3c49226a033fe46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:00:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Сүлжээний Visualizer апп барьж аваад харуулна зурвасын хэрэглээ буюу интервал дээр баригдсан нийт тоо аль гадна бодит цаг хугацаанд нь таны сүлжээг traversing мэдээллийн урсгал, харуулах эх үүсвэр, очих газар, протоколын мэдээлэл.';
+$lang['network_visualiser_app_name'] = 'Сүлжээний Visualizer';
+$lang['network_visualiser_display'] = 'Дэлгэц';
+$lang['network_visualiser_display_invalid'] = 'Дэлгэц нь хүчин төгөлдөр бус байна.';
+$lang['network_visualiser_interval'] = 'Интервал (секунд) шинэчил';
+$lang['network_visualiser_interval_is_invalid'] = 'Интервал хүчин төгөлдөр бус байна.';
+$lang['network_visualiser_report'] = 'тайлан Format';
+$lang['network_visualiser_report_detailed'] = 'Хүснэгтэн - дэлгэрүүлэн';
+$lang['network_visualiser_report_graphical'] = 'график';
+$lang['network_visualiser_report_invalid'] = 'Тайлангийн төрөл хүчин төгөлдөр бус байна.';
+$lang['network_visualiser_report_simple'] = 'Хүснэгтэн - Энгийн';
+$lang['network_visualiser_top_users'] = 'Топ хурд Хэрэглэгчид';
+$lang['network_visualiser_total_transfer'] = 'Нийт Дамжуулах';
+$lang['network_visualiser_traffic_summary'] = 'Сүлжээний Замын хөдөлгөөний тойм';
