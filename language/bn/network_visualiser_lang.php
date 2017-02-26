f5fd3ab275a604b54695bd2954b9a9d4b65f2da0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'নেটওয়ার্ক দৃষ্টিগোচরকারী অ্যাপ্লিকেশন যেমনটি এবং প্রদর্শন হয় ব্যান্ডউইথ ব্যবহার অথবা মোট ডেটা একটি ব্যবধান ধরে বন্দী ছাড়াও রিয়েল টাইমে আপনার নেটওয়ার্ক ঢোঁড়ন তথ্য প্রবাহ, প্রদর্শন উৎস, গন্তব্য এবং প্রোটোকল তথ্য.';
+$lang['network_visualiser_app_name'] = 'নেটওয়ার্ক দৃষ্টিগোচরকারী';
+$lang['network_visualiser_display'] = 'প্রদর্শন';
+$lang['network_visualiser_display_invalid'] = 'ডিসপ্লে অবৈধ.';
+$lang['network_visualiser_interval'] = 'আপডেট ব্যবধান (সেকেন্ড)';
+$lang['network_visualiser_interval_is_invalid'] = 'ব্যবধান অবৈধ.';
+$lang['network_visualiser_report'] = 'গালাগাল প্রতিবেদন বিন্যাস';
+$lang['network_visualiser_report_detailed'] = 'ট্যাবুলার - বিস্তারিত';
+$lang['network_visualiser_report_graphical'] = 'গ্রাফিক';
+$lang['network_visualiser_report_invalid'] = 'প্রতিবেদনের প্রকার অবৈধ.';
+$lang['network_visualiser_report_simple'] = 'ট্যাবুলার - সিম্পল';
+$lang['network_visualiser_top_users'] = 'শীর্ষ ব্যান্ডউইথ কনজিউমার্স';
+$lang['network_visualiser_total_transfer'] = 'মোট স্থানান্তর';
+$lang['network_visualiser_traffic_summary'] = 'নেটওয়ার্ক ট্রাফিক সংক্ষিপ্তসার';
