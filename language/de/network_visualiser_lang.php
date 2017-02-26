881c42367b164ee516f9c98ba4967ddf9454f736
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:56:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Die Netzwerk-Visualizer App erfasst und zeigt den Datenfluss Ihrem Netzwerk in Echtzeit durchlaufen, zeigt Quell-, Ziel- und Protokollinformationen zusätzlich entweder die Bandbreitennutzung oder insgesamt über ein Intervall erfassten Daten.';
+$lang['network_visualiser_app_name'] = 'Netzwerk Visualizer';
+$lang['network_visualiser_display'] = 'Anzeigen';
+$lang['network_visualiser_display_invalid'] = 'Anzeige ist ungültig.';
+$lang['network_visualiser_interval'] = 'Update-Intervall (Sekunden)';
+$lang['network_visualiser_interval_is_invalid'] = 'Intervall ist ungültig.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabellarische - Detaillierte';
+$lang['network_visualiser_report_graphical'] = 'Grafische';
+$lang['network_visualiser_report_invalid'] = 'Berichtstyp ist ungültig.';
+$lang['network_visualiser_report_simple'] = 'Tabellarische - Einfach';
+$lang['network_visualiser_top_users'] = 'Top Bandbreite Verbraucher';
+$lang['network_visualiser_total_transfer'] = 'Total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Zusammenfassung';
