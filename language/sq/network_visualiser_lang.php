ad385c75ee9143fd04c02bbf41deae5bd119327e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Të kap app Rrjeti visualizer dhe tregon rrjedhjen e të dhënave fre në rrjetin tuaj në kohë reale, burimi i shfaqur, destinacioni dhe informacionet protokoll përveç ose përdorimin e bandwidth apo të dhëna të përgjithshme kapur mbi një interval.';
+$lang['network_visualiser_app_name'] = 'Rrjeti Visualizer';
+$lang['network_visualiser_display'] = 'ekran';
+$lang['network_visualiser_display_invalid'] = 'Display është i pavlefshëm.';
+$lang['network_visualiser_interval'] = 'Update intervali (sekonda)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval është e pavlefshme.';
+$lang['network_visualiser_report'] = 'Raporti Format';
+$lang['network_visualiser_report_detailed'] = 'Tabelore - detajuar';
+$lang['network_visualiser_report_graphical'] = 'grafik';
+$lang['network_visualiser_report_invalid'] = 'Raporti lloj është i pavlefshëm.';
+$lang['network_visualiser_report_simple'] = 'Tabelore - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Konsumatorët';
+$lang['network_visualiser_total_transfer'] = 'Transfer Total';
+$lang['network_visualiser_traffic_summary'] = 'Rrjeti Përmbledhje Traffic';
