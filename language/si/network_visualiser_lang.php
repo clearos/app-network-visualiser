b49ef5f5e6562e1781f2b9cb658180a5c58cb7a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:02:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'ජාල විෂුවලයිසර් යෙදුම නිවැරදිව ග්රහණය දර්ශණය දත්ත ප්රවාහ එසැනින් ඔබේ ජාලය හරහා ගමන්කළ කලාප පළල භාවිතය හෝ නිවාඩුවක් ගන්නගිය මුළු දත්ත එක්කෝ අමතරව මූලාශ්රය, ගමනාන්තය සහ සන්ධාන තොරතුරු ප්රදර්ශනය.';
+$lang['network_visualiser_app_name'] = 'ජාල විෂුවලයිසර්';
+$lang['network_visualiser_display'] = 'ප්රදර්ශනය';
+$lang['network_visualiser_display_invalid'] = 'ප්රදර්ශනය වලංගු නැත.';
+$lang['network_visualiser_interval'] = 'යාවත්කාලීන පරතරය (තත්පර)';
+$lang['network_visualiser_interval_is_invalid'] = 'පරතරය වලංගු නැත.';
+$lang['network_visualiser_report'] = 'වාර්තා හැඩතල';
+$lang['network_visualiser_report_detailed'] = 'Tabular - විස්තරාත්මක';
+$lang['network_visualiser_report_graphical'] = 'චිත්රක';
+$lang['network_visualiser_report_invalid'] = 'වාර්තා වර්ගය වලංගු නැත.';
+$lang['network_visualiser_report_simple'] = 'Tabular - සරල';
+$lang['network_visualiser_top_users'] = 'Top කලාප පළල පාරිභෝගික';
+$lang['network_visualiser_total_transfer'] = 'මුළු මාරු';
+$lang['network_visualiser_traffic_summary'] = 'ජාල රථ වාහන සාරාංශය';
