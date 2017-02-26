39ae9f94144c6452ba02af6145bdca52e96c9331
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Network Visualizer app edər və göstərir bant genişliyi istifadə və ya bir interval üzərində əsir ümumi məlumatlara ya əlavə data real-vaxt sizin şəbəkə traversing axını tapılanlar mənbə, təyinat və protokol məlumat.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'göstərmək';
+$lang['network_visualiser_display_invalid'] = 'Display yanlışdır.';
+$lang['network_visualiser_interval'] = 'Interval (saniyə) yeniləyin';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval yanlışdır.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Cədvəlli - Ətraflı';
+$lang['network_visualiser_report_graphical'] = 'Qrafik';
+$lang['network_visualiser_report_invalid'] = 'Hesabat növü yanlışdır.';
+$lang['network_visualiser_report_simple'] = 'Cədvəlli - Simple';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth İstehlakçılar';
+$lang['network_visualiser_total_transfer'] = 'ümumi Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Traffic Xülasə';
