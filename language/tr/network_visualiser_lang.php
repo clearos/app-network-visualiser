b2e0d1be375c571980dacac8d18404d5c9fd8499
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Ağ görüntüleyici uygulaması yakalar ve görüntüler bant genişliği kullanımı veya bir aralık zarfında yakalanan toplam veri ya ek olarak veriler gerçek zamanlı olarak ağınızı kateden akışı, görüntüleme kaynak, hedef ve protokol bilgilerini.';
+$lang['network_visualiser_app_name'] = 'ağ görüntüleyici';
+$lang['network_visualiser_display'] = 'ekran';
+$lang['network_visualiser_display_invalid'] = 'Ekran geçersiz.';
+$lang['network_visualiser_interval'] = 'Aralık (saniye) güncelleyin';
+$lang['network_visualiser_interval_is_invalid'] = 'Aralıklı geçersiz.';
+$lang['network_visualiser_report'] = 'Rapor Formatı';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Detay';
+$lang['network_visualiser_report_graphical'] = 'grafik';
+$lang['network_visualiser_report_invalid'] = 'Rapor türü geçersiz.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Basit';
+$lang['network_visualiser_top_users'] = 'En Bant Genişliği Tüketiciler';
+$lang['network_visualiser_total_transfer'] = 'Toplam Transferi';
+$lang['network_visualiser_traffic_summary'] = 'Ağ Trafik Özeti';
