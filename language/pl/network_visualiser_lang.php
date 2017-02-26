074d2db61e90e354a45d686ef423326ca6d2e9cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:01:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Te zrzuty aplikacji Network Visualizer i wyświetla przepływ danych z poruszaniem sieci w czasie rzeczywistym, wyświetlanie źródła, przeznaczenia i Information Protocol oprócz obu Użycie przepustowości lub całkowitych danych zgromadzonych w trakcie okresu.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Pokaz';
+$lang['network_visualiser_display_invalid'] = 'Wyświetlacz jest nieprawidłowy.';
+$lang['network_visualiser_interval'] = 'Aktualizacja przedziale (sekund)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interwał jest nieprawidłowy.';
+$lang['network_visualiser_report'] = 'Format raportu';
+$lang['network_visualiser_report_detailed'] = 'Tabelaryczne - Szczegółowe';
+$lang['network_visualiser_report_graphical'] = 'Graficzny';
+$lang['network_visualiser_report_invalid'] = 'Typ raportu jest nieprawidłowy.';
+$lang['network_visualiser_report_simple'] = 'Tabelaryczne - Prosty';
+$lang['network_visualiser_top_users'] = 'Najważniejsze pasma Konsumenci';
+$lang['network_visualiser_total_transfer'] = 'Suma transferu';
+$lang['network_visualiser_traffic_summary'] = 'Ruch sieciowy Podsumowanie';
