eee84330976ccfecd14fb1e1ba9ac29de6448e16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:03:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Nu Network Pangatur visual ngarebut app jeung mintonkeun aliran data traversing jaringan Anjeun sacara real-time, mintonkeun sumber, tujuan jeung inpo protokol salian boh pamakéan rubakpita atawa total data direbut leuwih hiji interval.';
+$lang['network_visualiser_app_name'] = 'Network Pangatur visual';
+$lang['network_visualiser_display'] = 'mamerkeun';
+$lang['network_visualiser_display_invalid'] = 'Pintonan anu sah.';
+$lang['network_visualiser_interval'] = 'Apdet interval (detik)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval nyaéta sah.';
+$lang['network_visualiser_report'] = 'laporan Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - lengkep';
+$lang['network_visualiser_report_graphical'] = 'grafis';
+$lang['network_visualiser_report_invalid'] = 'Laporan tipe téh sah.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Basajan';
+$lang['network_visualiser_top_users'] = 'Bandwidth Konsumen Top';
+$lang['network_visualiser_total_transfer'] = 'total Transfer';
+$lang['network_visualiser_traffic_summary'] = 'Network Summary Traffic';
