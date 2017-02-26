b57e02929b328c7f6a56267fc530935e650091e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'The Network Visualizer app analanda kusonyezedwa deta otaya kuyendayenda maukonde anu mu zenizeni nthawi, kusonyeza gwero, kopita ndi mfundo protocol kuwonjezera kapena wakagwiritsidwe bandiwifi kapena deta okwana anagwira pa imeneyi ndi.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'Sonyezani';
+$lang['network_visualiser_display_invalid'] = 'Sonyezani ndi opanda pake.';
+$lang['network_visualiser_interval'] = 'Kuonjezera imeneyi (masekondi)';
+$lang['network_visualiser_interval_is_invalid'] = 'Imeneyi ili udzakhala osazindikilika.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabular - mwatsatanetsatane';
+$lang['network_visualiser_report_graphical'] = 'mawonekedwe';
+$lang['network_visualiser_report_invalid'] = 'Report mtundu ndi opanda pake.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Top bandiwifi Ogula';
+$lang['network_visualiser_total_transfer'] = 'Total Choka';
+$lang['network_visualiser_traffic_summary'] = 'Network Magalimoto Chidule';
