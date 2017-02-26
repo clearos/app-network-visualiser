0edd0def21b6bab05561cba291c0e4ad530b1d47
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:02:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'App zachytáva a zobrazuje tok dát prebiehajúce cez sieť v reálnom čase, z toho zobrazené zdroj, cieľ a Information Protocol okrem jednej využitie šírky pásma alebo celkových údajov zachytených v intervale Network Visualizer.';
+$lang['network_visualiser_app_name'] = 'Network Visualizer';
+$lang['network_visualiser_display'] = 'zobraziť';
+$lang['network_visualiser_display_invalid'] = 'Displej je neplatná.';
+$lang['network_visualiser_interval'] = 'Interval aktualizácie (sekundy)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval je neplatný.';
+$lang['network_visualiser_report'] = 'Report Format';
+$lang['network_visualiser_report_detailed'] = 'Tabuľková - Detailné';
+$lang['network_visualiser_report_graphical'] = 'grafický';
+$lang['network_visualiser_report_invalid'] = 'Typ správy je neplatný.';
+$lang['network_visualiser_report_simple'] = 'Tabuľková - Simple';
+$lang['network_visualiser_top_users'] = 'Najlepšie šírky pásma Spotrebitelia';
+$lang['network_visualiser_total_transfer'] = 'celkový prevod';
+$lang['network_visualiser_traffic_summary'] = 'Súhrn Traffic Network';
