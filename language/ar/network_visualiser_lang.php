cff31a762163e7784e2c90559ce7d93cf5dd23f9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:53:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'يلتقط التطبيق شبكة متخيل ويعرض تدفق البيانات التي تعبر الشبكة في الوقت الحقيقي، وعرض المصدر والمقصد والمعلومات بروتوكول بالإضافة إلى إما استخدام عرض النطاق الترددي أو مجموع البيانات التي تم التقاطها خلال فترة زمنية.';
+$lang['network_visualiser_app_name'] = 'شبكة متخيل';
+$lang['network_visualiser_display'] = 'عرض';
+$lang['network_visualiser_display_invalid'] = 'عرض غير صالح.';
+$lang['network_visualiser_interval'] = 'تحديث الفاصل الزمني (ثانية)';
+$lang['network_visualiser_interval_is_invalid'] = 'الفاصل الزمني غير صالح.';
+$lang['network_visualiser_report'] = 'شكل التقرير';
+$lang['network_visualiser_report_detailed'] = 'جدولي - مفصلة';
+$lang['network_visualiser_report_graphical'] = 'تخطيطي';
+$lang['network_visualiser_report_invalid'] = 'تقرير نوع غير صالح.';
+$lang['network_visualiser_report_simple'] = 'جدولي - بسيطة';
+$lang['network_visualiser_top_users'] = 'الأعلى للمستهلكين عرض النطاق الترددي';
+$lang['network_visualiser_total_transfer'] = 'اجمالي نقل';
+$lang['network_visualiser_traffic_summary'] = 'ملخص حركة مرور الشبكة';
