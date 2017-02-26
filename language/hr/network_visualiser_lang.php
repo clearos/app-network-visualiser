a09c7ba22ba024d1b4c294a96f5ad4ad951c2918
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:55:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Aplikacije koja snima i prikazuje protok podataka poprijeko svoje mreže u stvarnom vremenu, pokazujući izvor, odredište i informacije protokola pored jednog od širina pojasa običaj ili ukupnim podacima snimljenim tijekom intervala Network Visualizer.';
+$lang['network_visualiser_app_name'] = 'mreža Visualizer';
+$lang['network_visualiser_display'] = 'Prikaz';
+$lang['network_visualiser_display_invalid'] = 'Zaslon je nevažeći.';
+$lang['network_visualiser_interval'] = 'Interval ažuriranja (u sekundama)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval je nevažeći.';
+$lang['network_visualiser_report'] = 'Prijavi Format';
+$lang['network_visualiser_report_detailed'] = 'Tabelarni - Detaljna';
+$lang['network_visualiser_report_graphical'] = 'Grafički';
+$lang['network_visualiser_report_invalid'] = 'Vrsta izvješća nije valjan.';
+$lang['network_visualiser_report_simple'] = 'Tabelarni - Jednostavno';
+$lang['network_visualiser_top_users'] = 'Top Bandwidth Potrošači';
+$lang['network_visualiser_total_transfer'] = 'ukupno Prijenos';
+$lang['network_visualiser_traffic_summary'] = 'Promet Sažetak mreže';
