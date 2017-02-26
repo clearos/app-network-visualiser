c2ede43b83bd64fe236e195faf2a412d4b235df7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:54:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'Les captures d&#39;aplicacions de xarxa de visualitzadors i pantalles de flux de dades que travessa la seva xarxa en temps real, mostrant font, destinació i protocol d&#39;informació, a més de ja sigui l&#39;ús d&#39;ample de banda o total de dades capturades en un interval.';
+$lang['network_visualiser_app_name'] = 'xarxa visualitzador';
+$lang['network_visualiser_display'] = 'visualització';
+$lang['network_visualiser_display_invalid'] = 'Pantalla no és vàlid.';
+$lang['network_visualiser_interval'] = 'Interval d&#39;actualització (segons)';
+$lang['network_visualiser_interval_is_invalid'] = 'Interval no és vàlid.';
+$lang['network_visualiser_report'] = 'Format d&#39;informe';
+$lang['network_visualiser_report_detailed'] = 'Tabular - Detallat';
+$lang['network_visualiser_report_graphical'] = 'gràfic';
+$lang['network_visualiser_report_invalid'] = 'tipus d&#39;informe no és vàlid.';
+$lang['network_visualiser_report_simple'] = 'Tabular - Simple';
+$lang['network_visualiser_top_users'] = 'Els consumidors d&#39;ample de banda superiors';
+$lang['network_visualiser_total_transfer'] = 'transferència total';
+$lang['network_visualiser_traffic_summary'] = 'Resum de xarxa de tràfic';
