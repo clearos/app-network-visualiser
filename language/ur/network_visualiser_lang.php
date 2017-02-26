cfe0e8324daf83d3f0aa8e09c47b4e96711fb420
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:05:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/network_visualiser_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['network_visualiser_app_description'] = 'نیٹ ورک ویژوئلائزر اے پی پی کے قبضہ اور ڈسپلے بینڈوڈتھ کے استعمال یا ایک وقفہ کے دوران گرفتار کر لیا کل ڈیٹا یا تو اصل میں ڈیٹا کے بہاؤ کو اصل وقت میں آپ کے نیٹ ورک کی traversing، نمائش منبع، منزل اور پروٹوکول میں معلومات.';
+$lang['network_visualiser_app_name'] = 'نیٹ ورک ویژوئلائزر';
+$lang['network_visualiser_display'] = 'ڈسپلے';
+$lang['network_visualiser_display_invalid'] = 'ڈسپلے باطل ہے.';
+$lang['network_visualiser_interval'] = 'وقفہ (سیکنڈ) کی تازہ کاری کریں';
+$lang['network_visualiser_interval_is_invalid'] = 'وقفہ غلط ہے.';
+$lang['network_visualiser_report'] = 'رپورٹ کی شکل';
+$lang['network_visualiser_report_detailed'] = 'جدولی - تفصیلی';
+$lang['network_visualiser_report_graphical'] = 'گرافیکل';
+$lang['network_visualiser_report_invalid'] = 'رپورٹ قسم غلط ہے.';
+$lang['network_visualiser_report_simple'] = 'جدولی - سادہ';
+$lang['network_visualiser_top_users'] = 'اوپر بینڈوتھ صارفین';
+$lang['network_visualiser_total_transfer'] = 'کل منتقلی';
+$lang['network_visualiser_traffic_summary'] = 'نیٹ ورک ٹریفک کا خلاصہ';
